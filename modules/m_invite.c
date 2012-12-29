715b6856e7fe90d25d19762edeb4e3391e24787a
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2012-12-29 22:15:10 +0000

    - Backported -r1721 [m_invite.c: fixed broken CHWALL invite notice for invitees that are   on a remote server]
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.0.x@1722 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_invite.c
+++ modules/m_invite.c
@@ -125,21 +125,11 @@ m_invite(struct Client *client_p, struct Client *source_p,
 
     if (chptr->mode.mode & MODE_INVITEONLY)
     {
-      sendto_channel_local(CHFL_CHANOP, 0, chptr,
-                           ":%s NOTICE @%s :%s is inviting %s to %s.",
-                           me.name, chptr->chname, source_p->name,
-                           target_p->name, chptr->chname);
+      sendto_channel_butone(NULL, &me, chptr, CHFL_CHANOP,
+                            "NOTICE @%s :%s is inviting %s to %s.",
+                            chptr->chname, source_p->name,
+                            target_p->name, chptr->chname);
 
-      sendto_channel_remote(source_p, client_p, CHFL_CHANOP,
-                            CAP_TS6, NOCAPS, chptr,
-                            ":%s NOTICE @%s :%s is inviting %s to %s.",
-                            ID(&me), chptr->chname, source_p->name,
-                            target_p->name, chptr->chname);
-      sendto_channel_remote(source_p, client_p, CHFL_CHANOP,
-                            NOCAPS, CAP_TS6, chptr,
-                            ":%s NOTICE @%s :%s is inviting %s to %s.",
-                            me.name, chptr->chname, source_p->name,
-                            target_p->name, chptr->chname);
       /* Add the invite if channel is +i */
       add_invite(chptr, target_p);
     }

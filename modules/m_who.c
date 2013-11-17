78f1975fa551b3f452d5b5551add984feda09a6e
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2013-11-17 13:14:41 +0000

    - WHO now shows whether or not a nickname has been registered with NickServ. ('r' status flag)
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@2548 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_who.c
+++ modules/m_who.c
@@ -51,13 +51,15 @@ static void
 do_who(struct Client *source_p, struct Client *target_p,
        const char *chname, const char *op_flags)
 {
-  char status[7]; /* G*@%+\0 */
+  char status[8]; /* G*@%+\0 */
 
   if (HasUMode(source_p, UMODE_OPER))
-    snprintf(status, sizeof(status), "%c%s%s", target_p->away[0] ? 'G' : 'H',
+    snprintf(status, sizeof(status), "%c%s%s%s", target_p->away[0] ? 'G' : 'H',
+             HasUMode(target_p, UMODE_REGISTERED) ? "r" : "",
              HasUMode(target_p, UMODE_OPER) ? "*" : "", op_flags);
   else
-    snprintf(status, sizeof(status), "%c%s%s", target_p->away[0] ? 'G' : 'H',
+    snprintf(status, sizeof(status), "%c%s%s%s", target_p->away[0] ? 'G' : 'H',
+             HasUMode(target_p, UMODE_REGISTERED) ? "r" : "",
              HasUMode(target_p, UMODE_OPER) &&
              !HasUMode(target_p, UMODE_HIDDEN) ? "*" : "", op_flags);
 

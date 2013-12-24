1b4ee25b6abb60eba4701f9b89a238a88528a517
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2013-12-24 21:15:22 +0000

    - list.c:dlinkFindDelete(): minor readability improvements
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@2714 82007160-df01-0410-b94d-b575c5fd34c7

--- src/list.c
+++ src/list.c
@@ -244,35 +244,35 @@ dlink_move_node(dlink_node *m, dlink_list *list_del, dlink_list *list_add)
 dlink_node *
 dlinkFindDelete(dlink_list *list, void *data)
 {
-  dlink_node *m;
+  dlink_node *m = NULL;
 
   DLINK_FOREACH(m, list->head)
   {
-    if (m->data == data)
-    {
-      if (m->next)
-        m->next->prev = m->prev;
-      else
-      {
-        assert(list->tail == m);
-        list->tail = m->prev;
-      }
-      if (m->prev)
-        m->prev->next = m->next;
-      else
-      {
-        assert(list->head == m);
-        list->head = m->next;
-      }
-      /* Set this to NULL does matter */
-      m->next = m->prev = NULL;
-      list->length--;
+    if (m->data != data)
+      continue;
 
-      return m;
+    if (m->next)
+      m->next->prev = m->prev;
+    else
+    {
+      assert(list->tail == m);
+      list->tail = m->prev;
     }
+
+    if (m->prev)
+      m->prev->next = m->next;
+    else
+    {
+      assert(list->head == m);
+      list->head = m->next;
+    }
+
+    /* Set this to NULL does matter */
+    m->next = m->prev = NULL;
+    list->length--;
+
+    return m;
   }
 
   return NULL;
 }
-
-

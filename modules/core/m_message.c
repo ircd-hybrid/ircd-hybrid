0ef5ae6d09ff4c2a341f59ac3fed25ffe413b89e
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-01-09 21:05:03 +0000

    - m_message.c:msg_client(): removed pointless MyClient() test
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@2798 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_message.c
+++ modules/core/m_message.c
@@ -447,14 +447,12 @@ msg_client(int p_or_n, const char *command, struct Client *source_p,
        * and flooding    -- fl
        */
       if (!MyClient(source_p) || HasUMode(source_p, UMODE_OPER) ||
-          (MyClient(source_p) &&
-           !flood_attack_client(p_or_n, source_p, target_p)))
+          !flood_attack_client(p_or_n, source_p, target_p))
         sendto_anywhere(target_p, source_p, command, ":%s", text);
     }
   }
   else if (!MyClient(source_p) || HasUMode(source_p, UMODE_OPER) ||
-           (MyClient(source_p) &&
-            !flood_attack_client(p_or_n, source_p, target_p)))
+           !flood_attack_client(p_or_n, source_p, target_p))
     sendto_anywhere(target_p, source_p, command, ":%s", text);
 }
 

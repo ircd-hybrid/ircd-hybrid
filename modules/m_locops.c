dcc7d240f727a54547260caf27416c5e1dfa4600
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-01-15 23:27:00 +0000

    - m_locops.c:ms_locops(): replaced sendto_server() combo   with sendto_match_servs()
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@2825 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_locops.c
+++ modules/m_locops.c
@@ -75,10 +75,8 @@ ms_locops(struct Client *client_p, struct Client *source_p,
   if (parc != 3 || EmptyString(parv[2]))
     return 0;
 
-  sendto_server(client_p, CAP_TS6|CAP_CLUSTER, NOCAPS, ":%s LOCOPS %s :%s",
-                ID(source_p), parv[1], parv[2]);
-  sendto_server(client_p, CAP_CLUSTER, CAP_TS6, ":%s LOCOPS %s :%s",
-                source_p->name, parv[1], parv[2]);
+  sendto_match_servs(source_p, parv[1], CAP_KLN, "LOCOPS %s :%s",
+                     parv[1], parv[2]);
 
   if (!IsClient(source_p) || match(parv[1], me.name))
     return 0;

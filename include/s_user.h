487d0988c74ac85bd08f5f4b6b86f40890b0e267
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-01-28 16:40:30 +0000

    - register_remote_user(): removed unused realname/gecos parameter
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@2955 82007160-df01-0410-b94d-b575c5fd34c7

--- include/s_user.h
+++ include/s_user.h
@@ -47,7 +47,7 @@ extern void oper_up(struct Client *);
 extern void register_local_user(struct Client *);
 extern void register_remote_user(struct Client *,
                                  const char *, const char *,
-                                 const char *, const char *);
+                                 const char *);
 extern void init_uid(void);
 extern int valid_sid(const char *);
 extern int valid_hostname(const char *);

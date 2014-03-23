3ae8f7036933cf9d8905ee19f5aee6473ac9adb7
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-03-23 18:58:18 +0000

    - Backported -r3180 [m_locops.c:ms_locops(): accept LOCOPS from servers]
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@3191 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_locops.c
+++ modules/m_locops.c
@@ -78,7 +78,7 @@ ms_locops(struct Client *client_p, struct Client *source_p,
   sendto_match_servs(source_p, parv[1], CAP_CLUSTER, "LOCOPS %s :%s",
                      parv[1], parv[2]);
 
-  if (!IsClient(source_p) || match(parv[1], me.name))
+  if (match(parv[1], me.name))
     return 0;
 
   if (find_matching_name_conf(CONF_ULINE, source_p->servptr->name,

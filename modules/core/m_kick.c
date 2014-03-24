4dd37b31005f6fedd851296e692d9b20fe9e468b
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-03-24 19:42:47 +0000

    - m_kick.c: incorporate some of Adam's cleanups.
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3204 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_kick.c
+++ modules/core/m_kick.c
@@ -61,13 +61,8 @@ m_kick(struct Client *source_p, int parc, char *parv[])
     return 0;
   }
 
-  if (MyClient(source_p) && !IsFloodDone(source_p))
+  if (!IsFloodDone(source_p))
     flood_endgrace(source_p);
-
-  if (!EmptyString(parv[3]))
-    strlcpy(reason, parv[3], sizeof(reason));
-  else
-    strlcpy(reason, source_p->name, sizeof(reason));
 
   if ((chptr = hash_find_channel(parv[1])) == NULL)
   {
@@ -75,100 +70,107 @@ m_kick(struct Client *source_p, int parc, char *parv[])
     return 0;
   }
 
-  if (!IsServer(source_p) && !HasFlag(source_p, FLAGS_SERVICE))
+  if ((ms_source = find_channel_link(source_p, chptr)) == NULL)
   {
-    if ((ms_source = find_channel_link(source_p, chptr)) == NULL)
-    {
-      if (MyConnect(source_p))
-      {
-        sendto_one_numeric(source_p, &me, ERR_NOTONCHANNEL, chptr->chname);
-        return 0;
-      }
-    }
+    sendto_one_numeric(source_p, &me, ERR_NOTONCHANNEL, chptr->chname);
+    return 0;
+  }
 
-    if (!has_member_flags(ms_source, CHFL_CHANOP|CHFL_HALFOP))
-    {
-      /* was a user, not a server, and user isn't seen as a chanop here */
-      if (MyConnect(source_p))
-      {
-        /* user on _my_ server, with no chanops.. so go away */
-        sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, chptr->chname);
-        return 0;
-      }
-
-      if (chptr->channelts == 0)
-      {
-        /* If its a TS 0 channel, do it the old way */
-        sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, chptr->chname);
-        return 0;
-      }
-
-      /* Its a user doing a kick, but is not showing as chanop locally
-       * its also not a user ON -my- server, and the channel has a TS.
-       * There are two cases we can get to this point then...
-       *
-       *     1) connect burst is happening, and for some reason a legit
-       *        op has sent a KICK, but the SJOIN hasn't happened yet or
-       *        been seen. (who knows.. due to lag...)
-       *
-       *     2) The channel is desynced. That can STILL happen with TS
-       *
-       *     Now, the old code roger wrote, would allow the KICK to
-       *     go through. Thats quite legit, but lets weird things like
-       *     KICKS by users who appear not to be chanopped happen,
-       *     or even neater, they appear not to be on the channel.
-       *     This fits every definition of a desync, doesn't it? ;-)
-       *     So I will allow the KICK, otherwise, things are MUCH worse.
-       *     But I will warn it as a possible desync.
-       *
-       *     -Dianora
-       */
-    }
+  if (!has_member_flags(ms_source, CHFL_CHANOP|CHFL_HALFOP))
+  {
+    sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, chptr->chname);
+    return 0;
   }
 
   if ((target_p = find_chasing(source_p, parv[2])) == NULL)
+    return 0;  /* find_chasing sends ERR_NOSUCHNICK */
+
+  if (!(ms_target = find_channel_link(target_p, chptr)))
+  {
+    sendto_one_numeric(source_p, &me, ERR_USERNOTINCHANNEL, target_p->name, chptr->chname);
+    return 0;
+  }
+
+#ifdef HALFOPS
+  /* half ops cannot kick other halfops on private channels */
+  if (has_member_flags(ms_source, CHFL_HALFOP) && !has_member_flags(ms_source, CHFL_CHANOP))
+  {
+    if (((chptr->mode.mode & MODE_PRIVATE) && has_member_flags(ms_target,
+         CHFL_CHANOP|CHFL_HALFOP)) || has_member_flags(ms_target, CHFL_CHANOP))
+    {
+      sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, chptr->chname);
+      return 0;
+    }
+  }
+#endif
+  if (!EmptyString(parv[3]))
+    strlcpy(reason, parv[3], sizeof(reason));
+  else
+    strlcpy(reason, source_p->name, sizeof(reason));
+
+  sendto_channel_local(ALL_MEMBERS, 0, chptr, ":%s!%s@%s KICK %s %s :%s",
+                       source_p->name, source_p->username,
+                       source_p->host, chptr->chname,
+                       target_p->name, reason);
+  sendto_server(source_p, NOCAPS, NOCAPS, ":%s KICK %s %s :%s",
+                source_p->id, chptr->chname,
+                target_p->id, reason);
+  remove_user_from_channel(ms_target);
+  return 0;
+}
+
+/* ms_kick()
+ *  parv[0] = command
+ *  parv[1] = channel
+ *  parv[2] = client to kick
+ *  parv[3] = kick comment
+ */
+static int
+ms_kick(struct Client *source_p, int parc, char *parv[])
+{
+  char reason[KICKLEN + 1] = "";
+  struct Client *target_p = NULL;
+  struct Channel *chptr = NULL;
+  struct Membership *ms_target = NULL;
+
+  if (EmptyString(parv[2]))
     return 0;
 
-  if ((ms_target = find_channel_link(target_p, chptr)) != NULL)
-  {
-#ifdef HALFOPS
-    /* half ops cannot kick other halfops on private channels */
-    if (has_member_flags(ms_source, CHFL_HALFOP) && !has_member_flags(ms_source, CHFL_CHANOP))
-    {
-      if (((chptr->mode.mode & MODE_PRIVATE) && has_member_flags(ms_target,
-        CHFL_CHANOP|CHFL_HALFOP)) || has_member_flags(ms_target, CHFL_CHANOP))
-      {
-        sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, chptr->chname);
-        return 0;
-      }
-    }
-#endif
+  if ((chptr = hash_find_channel(parv[1])) == NULL)
+    return 0;
 
-    if (IsServer(source_p))
-      sendto_channel_local(ALL_MEMBERS, 0, chptr, ":%s KICK %s %s :%s",
-                           source_p->name, chptr->chname,
-                           target_p->name, reason);
-    else
-      sendto_channel_local(ALL_MEMBERS, 0, chptr, ":%s!%s@%s KICK %s %s :%s",
-                           source_p->name, source_p->username,
-                           source_p->host, chptr->chname,
-                           target_p->name, reason);
+  if ((target_p = hash_find_id(parv[2])) == NULL || !IsClient(target_p))
+    return 0;
 
-    sendto_server(source_p, NOCAPS, NOCAPS, ":%s KICK %s %s :%s",
-                  source_p->id, chptr->chname,
-                  target_p->id, reason);
-    remove_user_from_channel(ms_target);
-  }
+  if ((ms_target = find_channel_link(target_p, chptr)) == NULL)
+    return 0;
+
+  if (!EmptyString(parv[3]))
+    strlcpy(reason, parv[3], sizeof(reason));
   else
-    sendto_one_numeric(source_p, &me, ERR_USERNOTINCHANNEL, target_p->name, chptr->chname);
+    strlcpy(reason, source_p->name, sizeof(reason));
 
+  if (IsServer(source_p))
+    sendto_channel_local(ALL_MEMBERS, 0, chptr, ":%s KICK %s %s :%s",
+                         source_p->name, chptr->chname,
+                         target_p->name, reason);
+  else
+    sendto_channel_local(ALL_MEMBERS, 0, chptr, ":%s!%s@%s KICK %s %s :%s",
+                         source_p->name, source_p->username,
+                         source_p->host, chptr->chname,
+                         target_p->name, reason);
+
+  sendto_server(source_p, NOCAPS, NOCAPS, ":%s KICK %s %s :%s",
+                source_p->id, chptr->chname,
+                target_p->id, reason);
+  remove_user_from_channel(ms_target);
   return 0;
 }
 
 static struct Message kick_msgtab =
 {
   "KICK", 0, 0, 3, MAXPARA, MFLG_SLOW, 0,
-  { m_unregistered, m_kick, m_kick, m_ignore, m_kick, m_ignore }
+  { m_unregistered, m_kick, ms_kick, m_ignore, m_kick, m_ignore }
 };
 
 static void

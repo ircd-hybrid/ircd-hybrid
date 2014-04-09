9a7da8630b9f844d3af8b1f87f11e1e5edc88c19
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-04-09 20:01:25 +0000

    - m_list.c: update documentation
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3290 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_list.c
+++ modules/m_list.c
@@ -165,11 +165,17 @@ do_list(struct Client *source_p, int parc, char *parv[])
   safe_list_channels(source_p, no_masked_channels && lt->show_mask.head != NULL);
 }
 
-/*
-** mo_list
-**      parv[0] = command
-**      parv[1] = channel
-*/
+/*! \brief LIST command handler
+ *
+ * \param source_p Pointer to allocated Client struct from which the message
+ *                 originally comes from.  This can be a local or remote client.
+ * \param parc     Integer holding the number of supplied arguments.
+ * \param parv     Argument vector where parv[0] .. parv[parc-1] are non-NULL
+ *                 pointers.
+ * \note Valid arguments for this command are:
+ *      - parv[0] = command
+ *      - parv[1] = channel name/list options
+ */
 static int
 m_list(struct Client *source_p, int parc, char *parv[])
 {

a67597944d9a667c10c2e845199bc14ed2b662fe
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-04-15 11:41:24 +0000

    - Readded FLAGS_BLOCKED
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@3310 82007160-df01-0410-b94d-b575c5fd34c7

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -173,6 +173,7 @@ close_connection(struct Client *client_p)
      * even if it is marked as blocked (COMM_SELECT_READ handler is called
      * before COMM_SELECT_WRITE). Let's try, nothing to lose.. -adx
      */
+    DelFlag(client_p, FLAGS_BLOCKED);
     send_queued_write(client_p);
   }
 

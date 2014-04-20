3bae55d337bc95616e723d7baf47c2151e210b70
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-04-20 14:17:58 +0000

    - svn propset on m_unkline.c
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3351 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_unkline.c
+++ modules/m_unkline.c
@@ -21,7 +21,7 @@
 
 /*! \file m_unkline.c
  * \brief Includes required functions for processing the UNKLINE command.
- * \version $Id: m_unkline.c 3347 2014-04-20 14:03:06Z michael $
+ * \version $Id$
  */
 
 #include "stdinc.h"
@@ -228,7 +228,7 @@ struct module module_entry =
 {
   .node    = { NULL, NULL, NULL },
   .name    = NULL,
-  .version = "$Revision: 3347 $",
+  .version = "$Revision$",
   .handle  = NULL,
   .modinit = module_init,
   .modexit = module_exit,

3fa0b12bb9c4eeeb23bcc23a9ea2b334b50bfc9b
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-05-16 12:35:55 +0000

    - find_address_conf(): constification
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3553 82007160-df01-0410-b94d-b575c5fd34c7

--- include/hostmask.h
+++ include/hostmask.h
@@ -80,7 +80,7 @@ extern void clear_out_address_conf(void);
 extern void hostmask_expire_temporary(void);
 
 extern struct MaskItem *find_address_conf(const char *, const char *,
-                                            struct irc_ssaddr *, int, char *);
+                                          struct irc_ssaddr *, int, const char *);
 extern struct MaskItem *find_dline_conf(struct irc_ssaddr *, int);
 extern struct MaskItem *find_conf_by_address(const char *, struct irc_ssaddr *,
                                              unsigned int, int, const char *, const char *, int);

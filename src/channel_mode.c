bcafa9b67f10b1276336bdc382f34140965dd278
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-05-29 15:48:46 +0000

    - channel_mode.c:channel_modes(): get rid of trailing spaces if   the channel is +k/+l
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@3683 82007160-df01-0410-b94d-b575c5fd34c7

--- src/channel_mode.c
+++ src/channel_mode.c
@@ -318,7 +318,7 @@ channel_modes(struct Channel *chptr, struct Client *client_p,
     *mbuf++ = 'l';
 
     if (IsServer(client_p) || HasFlag(client_p, FLAGS_SERVICE) || IsMember(client_p, chptr))
-      pbuf += sprintf(pbuf, "%d ", chptr->mode.limit);
+      pbuf += sprintf(pbuf, " %d", chptr->mode.limit);
   }
 
   if (chptr->mode.key[0])
@@ -326,7 +326,7 @@ channel_modes(struct Channel *chptr, struct Client *client_p,
     *mbuf++ = 'k';
 
     if (IsServer(client_p) || HasFlag(client_p, FLAGS_SERVICE) || IsMember(client_p, chptr))
-      sprintf(pbuf, "%s ", chptr->mode.key);
+      sprintf(pbuf, " %s", chptr->mode.key);
   }
 
   *mbuf = '\0';

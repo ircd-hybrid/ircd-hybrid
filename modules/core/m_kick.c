b44e571d68fd8a19d552ec72ca790fb4b7962ae5
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-05-30 17:43:34 +0000

    - m_kick.c:m_kick(): channel halfops (%) may now no longer KICK other channel halfops
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3717 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_kick.c
+++ modules/core/m_kick.c
@@ -95,8 +95,7 @@ m_kick(struct Client *source_p, int parc, char *parv[])
   /* half ops cannot kick other halfops on private channels */
   if (has_member_flags(ms_source, CHFL_HALFOP) && !has_member_flags(ms_source, CHFL_CHANOP))
   {
-    if (((chptr->mode.mode & MODE_PRIVATE) && has_member_flags(ms_target,
-         CHFL_CHANOP|CHFL_HALFOP)) || has_member_flags(ms_target, CHFL_CHANOP))
+    if (has_member_flags(ms_target, CHFL_CHANOP|CHFL_HALFOP))
     {
       sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, chptr->chname);
       return 0;

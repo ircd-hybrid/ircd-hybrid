84bc85dceb302ad6e2cd1e17f89942fee094ea3c
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-06-02 18:59:39 +0000

    - m_tburst.c: doxygen
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3776 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_tmode.c
+++ modules/core/m_tmode.c
@@ -42,13 +42,18 @@
 #include "packet.h"
 
 
-/*
- * ms_tmode()
+/*! \brief TMODE command handler
  *
- * inputs	- parv[0] = command
- *		  parv[1] = TS
- *		  parv[2] = channel name
- *		  parv[3] = modestring
+ * \param source_p Pointer to allocated Client struct from which the message
+ *                 originally comes from.  This can be a local or remote client.
+ * \param parc     Integer holding the number of supplied arguments.
+ * \param parv     Argument vector where parv[0] .. parv[parc-1] are non-NULL
+ *                 pointers.
+ * \note Valid arguments for this command are:
+ *      - parv[0] = command
+ *      - parv[1] = timestamp
+ *      - parv[2] = channel name
+ *      - parv[3] = modes to be added or removed
  */
 static int
 ms_tmode(struct Client *source_p, int parc, char *parv[])

b46b5fc505f1e5a68e5c7c6ad94ace7c1e12fed9
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-06-08 13:49:41 +0000

    - m_join.c: removed do_join_0()
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3916 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_join.c
+++ modules/core/m_join.c
@@ -238,38 +238,6 @@ ms_join(struct Client *source_p, int parc, char *parv[])
   return 0;
 }
 
-/* do_join_0()
- *
- * inputs	- pointer to client doing join 0
- * output	- NONE
- * side effects	- Use has decided to join 0. This is legacy
- *		  from the days when channels were numbers not names. *sigh*
- *		  There is a bunch of evilness necessary here due to
- * 		  anti spambot code.
- */
-static void
-do_join_0(struct Client *source_p)
-{
-  dlink_node *ptr = NULL, *ptr_next = NULL;
-
-  if (source_p->channel.head)
-    if (MyConnect(source_p) && !HasUMode(source_p, UMODE_OPER))
-      check_spambot_warning(source_p, NULL);
-
-  DLINK_FOREACH_SAFE(ptr, ptr_next, source_p->channel.head)
-  {
-    struct Channel *chptr = ((struct Membership *)ptr->data)->chptr;
-
-    sendto_server(source_p, NOCAPS, NOCAPS, ":%s PART %s",
-                  source_p->id, chptr->chname);
-    sendto_channel_local(ALL_MEMBERS, 0, chptr, ":%s!%s@%s PART %s",
-                         source_p->name, source_p->username,
-                         source_p->host, chptr->chname);
-
-    remove_user_from_channel(ptr->data);
-  }
-}
-
 /* set_final_mode
  *
  * inputs       - channel mode

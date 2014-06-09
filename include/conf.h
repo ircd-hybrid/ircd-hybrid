9f158dc197b1ce0389bdcd640baf6b07db4bed01
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-06-09 13:34:36 +0000

    - Fixed bunch of compile warnings with -Wconversion
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@3925 82007160-df01-0410-b94d-b575c5fd34c7

--- include/conf.h
+++ include/conf.h
@@ -40,23 +40,23 @@
 #define IsConfGline(x)          ((x)->type == CONF_GLINE)
 
 /* MaskItem->flags */
-#define CONF_FLAGS_DO_IDENTD            0x00000001
-#define CONF_FLAGS_NO_TILDE             0x00000004
-#define CONF_FLAGS_NEED_IDENTD          0x00000008
-#define CONF_FLAGS_EXEMPTKLINE          0x00000010
-#define CONF_FLAGS_NOLIMIT              0x00000020
-#define CONF_FLAGS_SPOOF_IP             0x00000040
-#define CONF_FLAGS_SPOOF_NOTICE         0x00000080
-#define CONF_FLAGS_REDIR                0x00000100
-#define CONF_FLAGS_EXEMPTGLINE          0x00000200
-#define CONF_FLAGS_CAN_FLOOD            0x00000400
-#define CONF_FLAGS_NEED_PASSWORD        0x00000800
-#define CONF_FLAGS_ALLOW_AUTO_CONN      0x00001000
-#define CONF_FLAGS_ENCRYPTED            0x00002000
-#define CONF_FLAGS_IN_DATABASE          0x00004000
-#define CONF_FLAGS_EXEMPTRESV           0x00008000
-#define CONF_FLAGS_SSL                  0x00010000
-#define CONF_FLAGS_WEBIRC               0x00020000
+#define CONF_FLAGS_DO_IDENTD            0x00000001U
+#define CONF_FLAGS_NO_TILDE             0x00000004U
+#define CONF_FLAGS_NEED_IDENTD          0x00000008U
+#define CONF_FLAGS_EXEMPTKLINE          0x00000010U
+#define CONF_FLAGS_NOLIMIT              0x00000020U
+#define CONF_FLAGS_SPOOF_IP             0x00000040U
+#define CONF_FLAGS_SPOOF_NOTICE         0x00000080U
+#define CONF_FLAGS_REDIR                0x00000100U
+#define CONF_FLAGS_EXEMPTGLINE          0x00000200U
+#define CONF_FLAGS_CAN_FLOOD            0x00000400U
+#define CONF_FLAGS_NEED_PASSWORD        0x00000800U
+#define CONF_FLAGS_ALLOW_AUTO_CONN      0x00001000U
+#define CONF_FLAGS_ENCRYPTED            0x00002000U
+#define CONF_FLAGS_IN_DATABASE          0x00004000U
+#define CONF_FLAGS_EXEMPTRESV           0x00008000U
+#define CONF_FLAGS_SSL                  0x00010000U
+#define CONF_FLAGS_WEBIRC               0x00020000U
 
 /* Macros for struct MaskItem */
 #define IsConfWebIRC(x)         ((x)->flags & CONF_FLAGS_WEBIRC)
@@ -83,15 +83,15 @@
 /* shared/cluster server entry types
  * These defines are used for both shared and cluster.
  */
-#define SHARED_KLINE            0x0001
-#define SHARED_UNKLINE          0x0002
-#define SHARED_XLINE            0x0004
-#define SHARED_UNXLINE          0x0008
-#define SHARED_RESV             0x0010
-#define SHARED_UNRESV           0x0020
-#define SHARED_LOCOPS           0x0040
-#define SHARED_DLINE            0x0080
-#define SHARED_UNDLINE          0x0100
+#define SHARED_KLINE            0x00000001U
+#define SHARED_UNKLINE          0x00000002U
+#define SHARED_XLINE            0x00000004U
+#define SHARED_UNXLINE          0x00000008U
+#define SHARED_RESV             0x00000010U
+#define SHARED_UNRESV           0x00000020U
+#define SHARED_LOCOPS           0x00000040U
+#define SHARED_DLINE            0x00000080U
+#define SHARED_UNDLINE          0x00000100U
 #define SHARED_ALL              (SHARED_KLINE | SHARED_UNKLINE |\
                                  SHARED_XLINE | SHARED_UNXLINE |\
                                  SHARED_RESV | SHARED_UNRESV |\

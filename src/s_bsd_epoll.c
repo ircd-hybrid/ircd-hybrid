f0405a65ccdd70026a29ce62276fd89426c5cd99
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-06-18 13:24:48 +0000

    - s_bsd_epoll.c, s_bsd_kqueue.c: removed defines/prototypes for extremely outdated systems
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@3985 82007160-df01-0410-b94d-b575c5fd34c7

--- src/s_bsd_epoll.c
+++ src/s_bsd_epoll.c
@@ -36,61 +36,6 @@
 
 static fde_t efd;
 
-/* Thanks to ircu for the following ifdefs. */
-
-/* The GNU C library may have a valid header but stub implementations
- * of the epoll system calls.  If so, provide our own. */
-#if defined(__stub_epoll_create) || defined(__stub___epoll_create) || defined(EPOLL_NEED_BODY)
-
-/* Oh, did we mention that some glibc releases do not even define the
- * syscall numbers? */
-#if !defined(__NR_epoll_create)
-#if defined(__ia64__)
-#define __NR_epoll_create 1243
-#define __NR_epoll_ctl 1244
-#define __NR_epoll_wait 1245
-#elif defined(__x86_64__)
-#define __NR_epoll_create 214
-#define __NR_epoll_ctl 233
-#define __NR_epoll_wait 232
-#elif defined(__sparc64__) || defined(__sparc__)
-#define __NR_epoll_create 193
-#define __NR_epoll_ctl 194
-#define __NR_epoll_wait 195
-#elif defined(__s390__) || defined(__m68k__)
-#define __NR_epoll_create 249
-#define __NR_epoll_ctl 250
-#define __NR_epoll_wait 251
-#elif defined(__ppc64__) || defined(__ppc__)
-#define __NR_epoll_create 236
-#define __NR_epoll_ctl 237
-#define __NR_epoll_wait 238
-#elif defined(__parisc__) || defined(__arm26__) || defined(__arm__)
-#define __NR_epoll_create 224
-#define __NR_epoll_ctl 225
-#define __NR_epoll_wait 226
-#elif defined(__alpha__)
-#define __NR_epoll_create 407
-#define __NR_epoll_ctl 408
-#define __NR_epoll_wait 409
-#elif defined(__sh64__)
-#define __NR_epoll_create 282
-#define __NR_epoll_ctl 283
-#define __NR_epoll_wait 284
-#elif defined(__i386__) || defined(__sh__) || defined(__m32r__) || defined(__h8300__) || defined(__frv__)
-#define __NR_epoll_create 254
-#define __NR_epoll_ctl 255
-#define __NR_epoll_wait 256
-#else /* cpu types */
-#error No system call numbers defined for epoll family.
-#endif /* cpu types */
-#endif /* !defined(__NR_epoll_create) */
-
-_syscall1(int, epoll_create, int, size)
-_syscall4(int, epoll_ctl, int, epfd, int, op, int, fd, struct epoll_event *, event)
-_syscall4(int, epoll_wait, int, epfd, struct epoll_event *, pevents, int, maxevents, int, timeout)
-
-#endif /* epoll_create defined as stub */
 
 /*
  * init_netio

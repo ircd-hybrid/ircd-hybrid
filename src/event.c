62bce592f086ae90829a95d2d74873fc57c3c353
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-07-01 19:49:09 +0000

    - event.c, event.h: update copyright headers
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@4130 82007160-df01-0410-b94d-b575c5fd34c7

--- src/event.c
+++ src/event.c
@@ -1,11 +1,27 @@
 /*
- *  ircd-hybrid: an advanced Internet Relay Chat Daemon(ircd).
- *  event.c: Event functions.
+ *  ircd-hybrid: an advanced, lightweight Internet Relay Chat Daemon (ircd)
  *
- *  Copyright (c) 1998-2000 Regents of the University of California
  *  Copyright (c) 2000-2014 ircd-hybrid development team
  *
- *  $Id$
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ *  USA
+ */
+
+/*! \file event.c
+ * \brief Timer based event execution
+ * \version $Id$
  */
 
 #include "stdinc.h"

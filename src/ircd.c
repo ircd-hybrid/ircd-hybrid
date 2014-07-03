3c6897a8830aec1e70bc8603d54f653865b67e62
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-07-03 18:25:36 +0000

    - ircd.c:ssl_init(): removed SSL_OP_TLS_ROLLBACK_BUG from options
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@4160 82007160-df01-0410-b94d-b575c5fd34c7

--- src/ircd.c
+++ src/ircd.c
@@ -434,7 +434,7 @@ ssl_init(void)
   }
 
   SSL_CTX_set_options(ServerInfo.server_ctx, SSL_OP_NO_SSLv2|SSL_OP_NO_SSLv3|SSL_OP_NO_TLSv1);
-  SSL_CTX_set_options(ServerInfo.server_ctx, SSL_OP_TLS_ROLLBACK_BUG|SSL_OP_SINGLE_DH_USE);
+  SSL_CTX_set_options(ServerInfo.server_ctx, SSL_OP_SINGLE_DH_USE);
   SSL_CTX_set_verify(ServerInfo.server_ctx, SSL_VERIFY_PEER|SSL_VERIFY_CLIENT_ONCE,
                      always_accept_verify_cb);
   SSL_CTX_set_session_id_context(ServerInfo.server_ctx, session_id, sizeof(session_id) - 1);
@@ -462,7 +462,7 @@ ssl_init(void)
   }
 
   SSL_CTX_set_options(ServerInfo.client_ctx, SSL_OP_NO_SSLv2|SSL_OP_NO_SSLv3|SSL_OP_NO_TLSv1);
-  SSL_CTX_set_options(ServerInfo.client_ctx, SSL_OP_TLS_ROLLBACK_BUG|SSL_OP_SINGLE_DH_USE);
+  SSL_CTX_set_options(ServerInfo.client_ctx, SSL_OP_SINGLE_DH_USE);
   SSL_CTX_set_verify(ServerInfo.client_ctx, SSL_VERIFY_PEER|SSL_VERIFY_CLIENT_ONCE,
                      always_accept_verify_cb);
 #endif /* HAVE_LIBCRYPTO */

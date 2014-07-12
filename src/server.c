3d2371b178b1eb140a91e28425918c7ff140357f
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-07-12 17:52:08 +0000

    - server.c: cleaned up hunt_server()
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@4207 82007160-df01-0410-b94d-b575c5fd34c7

--- src/server.c
+++ src/server.c
@@ -157,81 +157,51 @@ hunt_server(struct Client *source_p, const char *command,
             const int server, const int parc, char *parv[])
 {
   struct Client *target_p = NULL;
-  struct Client *target_tmp = NULL;
-  dlink_node *ptr;
+  dlink_node *ptr = NULL;
 
   /* Assume it's me, if no server */
   if (parc <= server || EmptyString(parv[server]))
     return HUNTED_ISME;
 
-  if (!strcmp(parv[server], me.id) || !match(parv[server], me.name))
-    return HUNTED_ISME;
+  if ((target_p = find_person(source_p, parv[server])) == NULL)
+    target_p = hash_find_server(parv[server]);
 
-  /* These are to pickup matches that would cause the following
+  /*
+   * These are to pickup matches that would cause the following
    * message to go in the wrong direction while doing quick fast
    * non-matching lookups.
    */
-  if (MyClient(source_p))
-    target_p = hash_find_client(parv[server]);
-  else
-    target_p = find_person(source_p, parv[server]);
-
   if (target_p)
     if (target_p->from == source_p->from && !MyConnect(target_p))
       target_p = NULL;
 
-  if (target_p == NULL && (target_p = hash_find_server(parv[server])))
-    if (target_p->from == source_p->from && !MyConnect(target_p))
-      target_p = NULL;
-
-  /* Again, if there are no wild cards involved in the server
-   * name, use the hash lookup
-   */
-  if (target_p == NULL)
+  if (!target_p && has_wildcards(parv[server]))
   {
-    if (!has_wildcards(parv[server]))
+    DLINK_FOREACH(ptr, global_client_list.head)
     {
-      if (!(target_p = hash_find_server(parv[server])))
-      {
-        sendto_one_numeric(source_p, &me, ERR_NOSUCHSERVER, parv[server]);
-        return HUNTED_NOSUCH;
-      }
-    }
-    else
-    {
-      DLINK_FOREACH(ptr, global_client_list.head)
-      {
-        target_tmp = ptr->data;
+      struct Client *tmp = ptr->data;
 
-        if (!match(parv[server], target_tmp->name))
-        {
-          if (target_tmp->from == source_p->from && !MyConnect(target_tmp))
-            continue;
-          target_p = ptr->data;
+      assert(IsMe(tmp) || IsServer(tmp) || IsClient(tmp));
+      if (!match(parv[server], tmp->name))
+      {
+        if (tmp->from == source_p->from && !MyConnect(tmp))
+          continue;
+        target_p = ptr->data;
 
-          if (IsRegistered(target_p) && (target_p != source_p->from))
-            break;
-        }
+        if (target_p != source_p->from)  /* TBV: is this needed? */
+          break;
       }
     }
   }
 
   if (target_p)
   {
-    if (!IsRegistered(target_p))
-    {
-      sendto_one_numeric(source_p, &me, ERR_NOSUCHSERVER, parv[server]);
-      return HUNTED_NOSUCH;
-    }
-
+    assert(IsMe(target_p) || IsServer(target_p) || IsClient(target_p));
     if (IsMe(target_p) || MyClient(target_p))
       return HUNTED_ISME;
 
-    if (match(target_p->name, parv[server]))
-      parv[server] = target_p->name;
-
-    /* This is a little kludgy but should work... */
-    sendto_one(target_p, command, ID_or_name(source_p, target_p),
+    parv[server] = target_p->id;
+    sendto_one(target_p, command, source_p->id,
                parv[1], parv[2], parv[3], parv[4],
                parv[5], parv[6], parv[7], parv[8]);
     return HUNTED_PASS;

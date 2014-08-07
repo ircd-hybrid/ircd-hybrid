c3323f4036d124311226b87cedcc038f729ff02b
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-08-07 19:42:00 +0000

    - event.c: update show_events() to make /stats E look nicer
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@4432 82007160-df01-0410-b94d-b575c5fd34c7

--- src/event.c
+++ src/event.c
@@ -141,16 +141,16 @@ show_events(struct Client *source_p)
   const dlink_node *node;
 
   sendto_one_numeric(source_p, &me, RPL_STATSDEBUG|SND_EXPLICIT,
-                     "E :Operation                    Next Execution");
+                     "E :Operation                      Next Execution");
   sendto_one_numeric(source_p, &me, RPL_STATSDEBUG|SND_EXPLICIT,
-                     "E :-------------------------------------------");
+                     "E :---------------------------------------------");
 
   DLINK_FOREACH(node, events.head)
   {
     const struct event *ev = node->data;
 
     sendto_one_numeric(source_p, &me, RPL_STATSDEBUG|SND_EXPLICIT,
-                       "E :%-28s %-4d seconds",
+                       "E :%-30s %-4d seconds",
                        ev->name,
                        (int)(ev->next - CurrentTime));
   }

58d0b1f63c9417570fcbabfaa6966e55cdb6f0d9
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-08-09 19:07:40 +0000

    - auth.h: update documentation
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@4445 82007160-df01-0410-b94d-b575c5fd34c7

--- include/auth.h
+++ include/auth.h
@@ -27,8 +27,8 @@
 #ifndef INCLUDED_s_auth_h
 #define INCLUDED_s_auth_h
 
-#define RFC1413_BUFSIZ  512  /*!< rfc1413 says we MUST accept 512 bytes */
-#define RFC1413_PORT    113  /*!< As defined per rfc1413, IDENT server listens on TCP port 113 */
+#define RFC1413_BUFSIZ  512  /**< rfc1413 says we MUST accept 512 bytes */
+#define RFC1413_PORT    113  /**< As defined per rfc1413, IDENT server listens on TCP port 113 */
 
 #define AM_IN_AUTH      0x00000001U
 #define AM_DOING_AUTH   0x00000002U
@@ -51,11 +51,11 @@ struct Client;
 /** Stores state of the DNS and RFC 1413 ident lookups for a client. */
 struct AuthRequest
 {
-  dlink_node node;  /*!< auth_doing_list */
-  unsigned int flags;  /*!< Current state of request */
-  struct Client *client;  /*!< Pointer to Client structure for request */
-  fde_t fd;  /*!< File descriptor for auth queries */
-  time_t timeout;  /*!< Time when query expires */
+  dlink_node node;        /**< Doubly linked list node. */
+  unsigned int flags;     /**< Current state of request. */
+  struct Client *client;  /**< Pointer to Client structure for request. */
+  fde_t fd;               /**< File descriptor for identd queries. */
+  time_t timeout;         /**< Time when query expires. */
 };
 
 extern void auth_init(void);

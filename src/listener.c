de763220024e364d3c8d1a58100daf603b7ba0ff
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-08-17 20:20:19 +0000

    - listener.c: reformatting
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@4521 82007160-df01-0410-b94d-b575c5fd34c7

--- src/listener.c
+++ src/listener.c
@@ -38,7 +38,9 @@
 #include "send.h"
 #include "memory.h"
 
-static void accept_connection(fde_t *, void *);
+#define TOOFAST_WARNING "ERROR :Your host is trying to (re)connect too fast -- throttled.\r\n"
+#define DLINE_WARNING "ERROR :You have been D-lined.\r\n"
+
 static dlink_list listener_list;
 
 static struct Listener *
@@ -120,6 +122,80 @@ show_ports(struct Client *source_p)
   }
 }
 
+static void
+listener_accept_connection(fde_t *pfd, void *data)
+{
+  static time_t rate = 0;
+  struct irc_ssaddr addr;
+  int fd;
+  int pe;
+  struct Listener *listener = data;
+
+  assert(listener);
+
+  /* There may be many reasons for error return, but
+   * in otherwise correctly working environment the
+   * probable cause is running out of file descriptors
+   * (EMFILE, ENFILE or others?). The man pages for
+   * accept don't seem to list these as possible,
+   * although it's obvious that it may happen here.
+   * Thus no specific errors are tested at this
+   * point, just assume that connections cannot
+   * be accepted until some old is closed first.
+   */
+  while ((fd = comm_accept(listener, &addr)) != -1)
+  {
+    /*
+     * check for connection limit
+     */
+    if (number_fd > hard_fdlimit - 10)
+    {
+      ++ServerStats.is_ref;
+      sendto_realops_flags_ratelimited(&rate, "All connections in use. (%s)",
+                                       get_listener_name(listener));
+
+      if (!(listener->flags & LISTENER_SSL))
+        send(fd, "ERROR :All connections in use\r\n", 32, 0);
+
+      close(fd);
+      break;    /* jump out and re-register a new io request */
+    }
+
+    /*
+     * Do an initial check we aren't connecting too fast or with too many
+     * from this IP...
+     */
+    if ((pe = conf_connect_allowed(&addr, addr.ss.ss_family)))
+    {
+      ++ServerStats.is_ref;
+
+      if (!(listener->flags & LISTENER_SSL))
+      {
+        switch (pe)
+        {
+          case BANNED_CLIENT:
+            send(fd, DLINE_WARNING, sizeof(DLINE_WARNING)-1, 0);
+            break;
+          case TOO_FAST:
+            send(fd, TOOFAST_WARNING, sizeof(TOOFAST_WARNING)-1, 0);
+            break;
+        }
+      }
+
+      close(fd);
+      continue;    /* drop the one and keep on clearing the queue */
+    }
+
+    ++ServerStats.is_ac;
+    add_connection(listener, &addr, fd);
+  }
+
+  /* Re-register a new IO request for the next accept .. */
+  comm_setselect(&listener->fd, COMM_SELECT_READ, listener_accept_connection,
+                 listener, 0);
+}
+
+
 /*
  * inetport - create a listener socket in the AF_INET or AF_INET6 domain,
  * bind it to the port given in 'port' and listen to it
@@ -188,7 +264,7 @@ inetport(struct Listener *listener)
 
   /* Listen completion events are READ events .. */
 
-  accept_connection(&listener->fd, listener);
+  listener_accept_connection(&listener->fd, listener);
   return 1;
 }
 
@@ -354,80 +430,4 @@ add_listener(int port, const char *vhost_ip, unsigned int flags)
     listener->active = 1;
   else
     close_listener(listener);
-}
-
-#define TOOFAST_WARNING "ERROR :Your host is trying to (re)connect too fast -- throttled.\r\n"
-#define DLINE_WARNING "ERROR :You have been D-lined.\r\n"
-
-static void
-accept_connection(fde_t *pfd, void *data)
-{
-  static time_t rate = 0;
-  struct irc_ssaddr addr;
-  int fd;
-  int pe;
-  struct Listener *listener = data;
-
-  assert(listener);
-
-  /* There may be many reasons for error return, but
-   * in otherwise correctly working environment the
-   * probable cause is running out of file descriptors
-   * (EMFILE, ENFILE or others?). The man pages for
-   * accept don't seem to list these as possible,
-   * although it's obvious that it may happen here.
-   * Thus no specific errors are tested at this
-   * point, just assume that connections cannot
-   * be accepted until some old is closed first.
-   */
-  while ((fd = comm_accept(listener, &addr)) != -1)
-  {
-    /*
-     * check for connection limit
-     */
-    if (number_fd > hard_fdlimit - 10)
-    {
-      ++ServerStats.is_ref;
-      sendto_realops_flags_ratelimited(&rate, "All connections in use. (%s)",
-                                       get_listener_name(listener));
-
-      if (!(listener->flags & LISTENER_SSL))
-        send(fd, "ERROR :All connections in use\r\n", 32, 0);
-
-      close(fd);
-      break;    /* jump out and re-register a new io request */
-    }
-
-    /*
-     * Do an initial check we aren't connecting too fast or with too many
-     * from this IP...
-     */
-    if ((pe = conf_connect_allowed(&addr, addr.ss.ss_family)))
-    {
-      ++ServerStats.is_ref;
-
-      if (!(listener->flags & LISTENER_SSL))
-      {
-        switch (pe)
-        {
-          case BANNED_CLIENT:
-            send(fd, DLINE_WARNING, sizeof(DLINE_WARNING)-1, 0);
-            break;
-          case TOO_FAST:
-            send(fd, TOOFAST_WARNING, sizeof(TOOFAST_WARNING)-1, 0);
-            break;
-        }
-      }
-
-      close(fd);
-      continue;    /* drop the one and keep on clearing the queue */
-    }
-
-    ++ServerStats.is_ac;
-    add_connection(listener, &addr, fd);
-  }
-
-  /* Re-register a new IO request for the next accept .. */
-  comm_setselect(&listener->fd, COMM_SELECT_READ, accept_connection,
-                 listener, 0);
 }

66f030956605e9f9688a8136bb471faaf66697d5
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-09-22 15:43:00 +0000

    - m_dline.c: fixed broken /dline remote.server
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.1.x@4651 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_dline.c
+++ modules/m_dline.c
@@ -449,7 +449,7 @@ static int
 ms_undline(struct Client *client_p, struct Client *source_p,
            int parc, char *parv[])
 {
-  const char *addr = parv[1];
+  const char *addr = parv[2];
 
   if (parc != 3 || EmptyString(parv[2]))
     return 0;

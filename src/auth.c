fb80716c2eb1cd7ecf96c9922c79bdbbd9d1e776
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-10-03 15:28:55 +0000

    - auth.c:start_auth_query(): removed extraneous ilog(). report_error() already does the logging
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@4699 82007160-df01-0410-b94d-b575c5fd34c7

--- src/auth.c
+++ src/auth.c
@@ -231,8 +231,6 @@ start_auth_query(struct AuthRequest *auth)
   {
     report_error(L_ALL, "creating auth stream socket %s:%s",
                  get_client_name(auth->client, SHOW_IP), errno);
-    ilog(LOG_TYPE_IRCD, "Unable to create auth socket for %s",
-        get_client_name(auth->client, SHOW_IP));
     ++ServerStats.is_abad;
     return 0;
   }

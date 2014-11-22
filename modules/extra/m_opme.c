b92c494eff12252a29907a9e28dcf0d69220fd71
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-11-22 19:09:46 +0000

    - m_opme.c:ms_opme(): use TMODE instead of MODE
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@4903 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/extra/m_opme.c
+++ modules/extra/m_opme.c
@@ -101,7 +101,8 @@ mo_opme(struct Client *source_p, int parc, char *parv[])
 
   AddMemberFlag(member, CHFL_CHANOP);
   sendto_channel_local(0, chptr, ":%s MODE %s +o %s", me.name, chptr->name, source_p->name);
-  sendto_server(NULL, 0, 0, ":%s MODE %s +o %s", me.id, chptr->name, source_p->id);
+  sendto_server(NULL, 0, 0, ":%s TMODE %lu %s +o %s", me.id, (unsigned long)chptr->creationtime,
+                chptr->name, source_p->id);
   return 0;
 }
 

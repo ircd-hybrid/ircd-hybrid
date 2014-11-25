c2efeb3993ccf746cebd11175e0c0a867904aebc
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-11-25 14:37:41 +0000

    - send.c: unbreak and sanitize send_message_remote()
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@4922 82007160-df01-0410-b94d-b575c5fd34c7

--- src/send.c
+++ src/send.c
@@ -133,38 +133,14 @@ static void
 send_message_remote(struct Client *to, struct Client *from, struct dbuf_block *buf)
 {
   assert(MyConnect(to));
+  assert(!IsMe(to));
+  assert(to->from == to);
 
-  /* Optimize by checking if (from && to) before everything */
-  /* we set to->from up there.. */
-
-  if (!MyClient(from) && IsClient(to) && (to == from->from))
+  if (!MyConnect(from) && to == from->from)
   {
-    if (IsServer(from))
-    {
-      sendto_realops_flags(UMODE_ALL, L_ALL, SEND_NOTICE,
-                           "Send message to %s [%s] dropped from %s(Fake Dir)",
-                           to->name, to->from->name, from->name);
-      return;
-    }
-
     sendto_realops_flags(UMODE_ALL, L_ALL, SEND_NOTICE,
-                         "Ghosted: %s[%s@%s] from %s[%s@%s] (%s)",
-                         to->name, to->username, to->host,
-                         from->name, from->username, from->host,
-                         to->from->name);
-
-    sendto_server(NULL, NOCAPS, NOCAPS,
-                  ":%s KILL %s :%s (%s[%s@%s] Ghosted %s)",
-                  me.id, to->id, me.name, to->name,
-                  to->username, to->host, to->from->name);
-
-    AddFlag(to, FLAGS_KILLED);
-
-    if (IsClient(from))
-      sendto_one_numeric(from, &me, ERR_GHOSTEDCLIENT, to->name,
-                         to->username, to->host, to->from);
-
-    exit_client(to, "Ghosted client");
+                         "Send message to %s dropped from %s (Fake Dir)",
+                         to->name, from->name);
     return;
   }
 

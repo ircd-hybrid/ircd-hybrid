9e1fa4e8373e13c83067540ed9e200fc48e47229
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2014-12-09 15:29:56 +0000

    - motd.c:motd_create(): fixed indentation
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@5011 82007160-df01-0410-b94d-b575c5fd34c7

--- src/motd.c
+++ src/motd.c
@@ -67,14 +67,14 @@ motd_create(const char *mask, const char *path)
     switch (parse_netmask(mask, &tmp->address, &tmp->addrbits))
     {
       case HM_IPV4:
-      tmp->type = MOTD_IPMASKV4;
-      break;
-    case HM_IPV6:
-      tmp->type = MOTD_IPMASKV6;
-      break;
-    default: /* HM_HOST */
-      tmp->type = MOTD_HOSTMASK;
-      break;
+        tmp->type = MOTD_IPMASKV4;
+        break;
+      case HM_IPV6:
+        tmp->type = MOTD_IPMASKV6;
+        break;
+      default:  /* HM_HOST */
+        tmp->type = MOTD_HOSTMASK;
+        break;
     }
   }
 

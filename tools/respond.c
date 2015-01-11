012b286214350ed894ca4acf0c996716d6a1de45
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-01-11 12:41:14 +0000

    - Update copyright years
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@5346 82007160-df01-0410-b94d-b575c5fd34c7

--- tools/respond.c
+++ tools/respond.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2001-2014 ircd-hybrid development team
+ *  Copyright (c) 2001-2015 ircd-hybrid development team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

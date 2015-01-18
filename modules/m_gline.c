a45aad5bf3caf664d8e004bd91421409524e12fb
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-01-18 12:42:23 +0000

    - m_gline.c, m_kline.c, motd.c: need to check against client->sockhost as well
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@5386 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_gline.c
+++ modules/m_gline.c
@@ -75,7 +75,7 @@ check_gline(struct AddressRec *arec)
             conf_try_ban(client_p, arec->conf);
         break;
       default:  /* HM_HOST */
-        if (!match(arec->Mask.hostname, client_p->host))
+        if (!match(arec->Mask.hostname, client_p->host) || !match(arec->Mask.hostname, client_p->sockhost))
           conf_try_ban(client_p, arec->conf);
         break;
     }

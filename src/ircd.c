0c991461d9f0935beee1a40c4bba89f9b66493a4
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-02-14 20:15:46 +0000

    - ircd.c:write_pidfile(): cosmetical fixes to fopen/fputs error messages to be consistent with all other error messages
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@5566 82007160-df01-0410-b94d-b575c5fd34c7

--- src/ircd.c
+++ src/ircd.c
@@ -312,13 +312,13 @@ write_pidfile(const char *filename)
     snprintf(buff, sizeof(buff), "%u\n", pid);
 
     if (fputs(buff, fb) == -1)
-      ilog(LOG_TYPE_IRCD, "Error writing %u to pid file %s (%s)",
+      ilog(LOG_TYPE_IRCD, "Error writing %u to pid file %s: %s",
            pid, filename, strerror(errno));
 
     fclose(fb);
   }
   else
-    ilog(LOG_TYPE_IRCD, "Error opening pid file %s (%s)",
+    ilog(LOG_TYPE_IRCD, "Error opening pid file %s: %s",
          filename, strerror(errno));
 }
 

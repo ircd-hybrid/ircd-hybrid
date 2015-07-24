8b5607046fc7bcd9321839ec4259bd58fa173cb9
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-07-24 13:22:49 +0000

    - Move show_events() from event.c to m_stats.c. Remove now unused prototypes.
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@6305 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_stats.c
+++ modules/m_stats.c
@@ -639,7 +639,22 @@ stats_exempt(struct Client *source_p, int parc, char *parv[])
 static void
 stats_events(struct Client *source_p, int parc, char *parv[])
 {
-  show_events(source_p);
+  const dlink_node *node;
+
+  sendto_one_numeric(source_p, &me, RPL_STATSDEBUG | SND_EXPLICIT,
+                     "E :Operation                      Next Execution");
+  sendto_one_numeric(source_p, &me, RPL_STATSDEBUG | SND_EXPLICIT,
+                     "E :---------------------------------------------");
+
+  DLINK_FOREACH(node, event_get_list()->head)
+  {
+    const struct event *ev = node->data;
+
+    sendto_one_numeric(source_p, &me, RPL_STATSDEBUG | SND_EXPLICIT,
+                       "E :%-30s %-4d seconds",
+                       ev->name,
+                       (int)(ev->next - CurrentTime));
+  }
 }
 
 static void

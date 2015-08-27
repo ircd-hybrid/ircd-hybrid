4bc4244ebcb73ba362b3995d57923a14ba7b7f44
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-08-27 10:44:15 +0000

    - hostmask.h, hostmask.c: use const keyword where appropriate
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@6429 82007160-df01-0410-b94d-b575c5fd34c7

--- include/hostmask.h
+++ include/hostmask.h
@@ -78,9 +78,9 @@ extern void delete_one_address_conf(const char *, struct MaskItem *);
 extern void clear_out_address_conf(void);
 extern void hostmask_expire_temporary(void);
 
-extern struct MaskItem *find_address_conf(const char *, const char *,
-                                          struct irc_ssaddr *, int, const char *);
-extern struct MaskItem *find_dline_conf(struct irc_ssaddr *, int);
+extern struct MaskItem *find_address_conf(const char *, const char *, const struct irc_ssaddr *,
+                                          int, const char *);
+extern struct MaskItem *find_dline_conf(const struct irc_ssaddr *, int);
 extern struct MaskItem *find_conf_by_address(const char *, const struct irc_ssaddr *,
                                              unsigned int, int, const char *, const char *, int);
 #endif /* INCLUDED_hostmask_h */

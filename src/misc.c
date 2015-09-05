13bc61933adc1fda8a8b5605228e898e4f4db661
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-09-05 17:47:55 +0000

    - misc.c: cleanup date() and make it use strftime()
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@6499 82007160-df01-0410-b94d-b575c5fd34c7

--- src/misc.c
+++ src/misc.c
@@ -30,63 +30,21 @@
 #include "ircd.h"
 
 
-static const char *const months[] =
-{
-  "January",   "February", "March",    "April",
-  "May",       "June",     "July",     "August",
-  "September", "October",  "November", "December"
-};
-
-static const char *const weekdays[] =
-{
-  "Sunday",   "Monday", "Tuesday", "Wednesday",
-  "Thursday", "Friday", "Saturday"
-};
-
 const char *
 date(time_t lclock)
 {
-  static char buf[80], plus;
+  static char buf[80];
   static time_t lclock_last;
-  struct tm *lt, *gm;
-  struct tm gmbuf;
-  int minswest;
 
   if (!lclock)
     lclock = CurrentTime;
 
-  if (lclock_last == lclock)
-    return buf;
-
-  lclock_last = lclock;
-  gm = gmtime(&lclock);
-  memcpy(&gmbuf, gm, sizeof(gmbuf));
-  gm = &gmbuf;
-  lt = localtime(&lclock);
-
-  /*
-   * There is unfortunately no clean portable way to extract time zone
-   * offset information, so do ugly things.
-   */
-  minswest = (gm->tm_hour - lt->tm_hour) * 60 + (gm->tm_min - lt->tm_min);
-
-  if (lt->tm_yday != gm->tm_yday)
+  if (lclock_last != lclock)
   {
-    if ((lt->tm_yday > gm->tm_yday && lt->tm_year == gm->tm_year) ||
-        (lt->tm_yday < gm->tm_yday && lt->tm_year != gm->tm_year))
-      minswest -= 24 * 60;
-    else
-      minswest += 24 * 60;
+    lclock_last = lclock;
+    strftime(buf, sizeof(buf), "%A %B %-e %Y -- %T %z", localtime(&lclock));
   }
 
-  plus = (minswest > 0) ? '-' : '+';
-  if (minswest < 0)
-    minswest = -minswest;
-
-  snprintf(buf, sizeof(buf), "%s %s %d %d -- %02u:%02u:%02u %c%02u:%02u",
-           weekdays[lt->tm_wday], months[lt->tm_mon], lt->tm_mday,
-           lt->tm_year + 1900, lt->tm_hour, lt->tm_min, lt->tm_sec,
-           plus, minswest/60, minswest%60);
   return buf;
 }
 

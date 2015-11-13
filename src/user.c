75331858404ffd4b3fb4983d798aaa15b1cdec9e
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-11-13 20:32:36 +0000

    - user.c:user_set_hostmask(): really should be using sendto_common_channels_local() instead of sendto_channel_local() for sending AWAY,   otherwise we would end up sending multiple AWAY messages to clients.
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.3.x@6779 82007160-df01-0410-b94d-b575c5fd34c7

--- src/user.c
+++ src/user.c
@@ -827,10 +827,11 @@ user_set_hostmask(struct Client *target_p, const char *hostname, const int what)
       sendto_channel_local(target_p, member->chptr, 0, 0, CAP_CHGHOST, ":%s MODE %s +%s %s",
                            target_p->servptr->name, member->chptr->name,
                            modebuf, nickbuf);
-    if (target_p->away[0])
-      sendto_channel_local(target_p, member->chptr, 0, CAP_AWAY_NOTIFY, CAP_CHGHOST,
-                           ":%s!%s@%s AWAY :%s",
-                           target_p->name, target_p->username,
-                           target_p->host, target_p->away);
   }
+
+  if (target_p->away[0])
+    sendto_common_channels_local(target_p, 0, CAP_AWAY_NOTIFY, CAP_CHGHOST,
+                                 ":%s!%s@%s AWAY :%s",
+                                 target_p->name, target_p->username,
+                                 target_p->host, target_p->away);
 }

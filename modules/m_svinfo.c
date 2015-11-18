ece5b5264e05de53f1d90892e7313796fd179a8c
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-11-18 11:00:46 +0000

    - m_svinfo.c: style corrections
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@6797 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_svinfo.c
+++ modules/m_svinfo.c
@@ -65,11 +65,12 @@ ms_svinfo(struct Client *source_p, int parc, char *parv[])
      * we drop the link  -orabidoo
      */
     sendto_realops_flags(UMODE_SERVNOTICE, L_ADMIN, SEND_NOTICE,
-            "Link %s dropped, wrong TS protocol version (%s,%s)",
-            get_client_name(source_p, SHOW_IP), parv[1], parv[2]);
+          "Link %s dropped, wrong TS protocol version (%s,%s)",
+          get_client_name(source_p, SHOW_IP), parv[1], parv[2]);
     sendto_realops_flags(UMODE_SERVNOTICE, L_OPER, SEND_NOTICE,
-                 "Link %s dropped, wrong TS protocol version (%s,%s)",
-                 get_client_name(source_p, MASK_IP), parv[1], parv[2]);
+          "Link %s dropped, wrong TS protocol version (%s,%s)",
+          get_client_name(source_p, MASK_IP), parv[1], parv[2]);
+
     exit_client(source_p, "Incompatible TS version");
     return 0;
   }
@@ -86,33 +87,22 @@ ms_svinfo(struct Client *source_p, int parc, char *parv[])
   {
     sendto_realops_flags(UMODE_SERVNOTICE, L_ADMIN, SEND_NOTICE,
           "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
-          get_client_name(source_p, SHOW_IP),
-          CurrentTime,
-          theirtime,
-          deltat);
+          get_client_name(source_p, SHOW_IP), CurrentTime, theirtime, deltat);
     sendto_realops_flags(UMODE_SERVNOTICE, L_OPER, SEND_NOTICE,
           "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
-           get_client_name(source_p, MASK_IP),
-           CurrentTime,
-           theirtime,
-           deltat);
+           get_client_name(source_p, MASK_IP), CurrentTime, theirtime, deltat);
     ilog(LOG_TYPE_IRCD,
          "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
-         get_client_name(source_p, SHOW_IP),
-         CurrentTime,
-         theirtime,
-         deltat);
+         get_client_name(source_p, SHOW_IP), CurrentTime, theirtime, deltat);
+
     exit_client(source_p, "Excessive TS delta");
     return 0;
   }
 
   if (deltat > ConfigGeneral.ts_warn_delta)
     sendto_realops_flags(UMODE_SERVNOTICE, L_ALL, SEND_NOTICE,
-                "Link %s notable TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
-                source_p->name,
-                CurrentTime,
-                theirtime,
-                deltat);
+          "Link %s notable TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
+          source_p->name, CurrentTime, theirtime, deltat);
   return 0;
 }
 

e7a05c3fe4d54dc9ea93053acc06dee9c4a0d56b
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2015-11-30 20:36:22 +0000

    - m_svinfo.c: whitespace changes
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@6876 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_svinfo.c
+++ modules/m_svinfo.c
@@ -89,11 +89,11 @@ ms_svinfo(struct Client *source_p, int parc, char *parv[])
   if (deltat > ConfigGeneral.ts_max_delta)
   {
     sendto_realops_flags(UMODE_SERVNOTICE, L_ADMIN, SEND_NOTICE,
-          "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
-          get_client_name(source_p, SHOW_IP), CurrentTime, theirtime, deltat);
+         "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
+         get_client_name(source_p, SHOW_IP), CurrentTime, theirtime, deltat);
     sendto_realops_flags(UMODE_SERVNOTICE, L_OPER, SEND_NOTICE,
-          "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
-           get_client_name(source_p, MASK_IP), CurrentTime, theirtime, deltat);
+         "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
+          get_client_name(source_p, MASK_IP), CurrentTime, theirtime, deltat);
     ilog(LOG_TYPE_IRCD,
          "Link %s dropped, excessive TS delta (my TS=%ju, their TS=%ju, delta=%ji)",
          get_client_name(source_p, SHOW_IP), CurrentTime, theirtime, deltat);

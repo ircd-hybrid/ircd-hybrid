cedefa41a4161107f4ccf0202696482286db45ea
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-01-28 11:41:59 +0000

    - tls*: more copyright fixups
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@7164 82007160-df01-0410-b94d-b575c5fd34c7

--- include/tls_openssl.h
+++ include/tls_openssl.h
@@ -1,7 +1,9 @@
 /*
  *  ircd-hybrid: an advanced, lightweight Internet Relay Chat Daemon (ircd)
  *
- *  Copyright (c) 2006-2016 ircd-hybrid development team
+ *  Copyright (c) 2015 Attila Molnar <attilamolnar@hush.com>
+ *  Copyright (c) 2015 Adam <Adam@anope.org>
+ *  Copyright (c) 2015-2016 ircd-hybrid development team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
--- src/tls_none.c
+++ src/tls_none.c
@@ -1,6 +1,8 @@
 /*
  *  ircd-hybrid: an advanced, lightweight Internet Relay Chat Daemon (ircd)
  *
+ *  Copyright (c) 2015 Attila Molnar <attilamolnar@hush.com>
+ *  Copyright (c) 2015 Adam <Adam@anope.org>
  *  Copyright (c) 2015-2016 ircd-hybrid development team
  *
  *  This program is free software; you can redistribute it and/or modify

10455f3f17aae00c7c8e03f56da3529582eaaea2
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-01-30 18:54:24 +0000

    - tls_gnutls.c: add %SERVER_PRECEDENCE to priorities
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@7193 82007160-df01-0410-b94d-b575c5fd34c7

--- src/tls_gnutls.c
+++ src/tls_gnutls.c
@@ -73,7 +73,9 @@ tls_new_cred(void)
     return 0;
   }
 
-  gnutls_priority_init(&context->priorities, "NORMAL", NULL);
+  /* TBD: set ciphers based on serverinfo::ssl_cipher_list */
+
+  gnutls_priority_init(&context->priorities, "NORMAL:%SERVER_PRECEDENCE", NULL);
 
   ret = gnutls_certificate_set_x509_key_file(context->x509_cred, ConfigServerInfo.ssl_certificate_file, ConfigServerInfo.rsa_private_key_file, GNUTLS_X509_FMT_PEM);
   if (ret != GNUTLS_E_SUCCESS)
@@ -243,9 +245,9 @@ tls_set_ciphers(tls_data_t *tls_data, const char *cipher_list)
   if (ret != GNUTLS_E_SUCCESS)
   {
     /* GnuTLS did not understand the user supplied string, log and fall back to the default priorities */
-    ilog(LOG_TYPE_IRCD, "Failed to set GnuTLS priorities to \"%s\": %s Syntax error at position %u, falling back to default (NORMAL)",
+    ilog(LOG_TYPE_IRCD, "Failed to set GnuTLS priorities to \"%s\": %s Syntax error at position %u, falling back to default (NORMAL:%%SERVER_PRECEDENCE)",
          cipher_list, gnutls_strerror(ret), (unsigned int)(prioerror - cipher_list));
-    gnutls_priority_init(&tls_data->context->priorities, "NORMAL", NULL);
+    gnutls_priority_init(&tls_data->context->priorities, "NORMAL:%SERVER_PRECEDENCE", NULL);
     return 0;
   }
 

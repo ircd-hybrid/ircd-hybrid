9dc8f472ac5087b80732b992ddb81c411909086f
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-03-07 16:57:15 +0000

    - m_stats.c:report_shared(), report_cluster(): increase buffer size for the extra 'c' character
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@7425 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_stats.c
+++ modules/m_stats.c
@@ -76,7 +76,7 @@ report_shared(struct Client *source_p)
   };
 
   dlink_node *node;
-  char buf[sizeof(flag_table) / sizeof(struct shared_types)];
+  char buf[sizeof(flag_table) / sizeof(struct shared_types) + 1];  /* +1 for 'c' */
 
   DLINK_FOREACH(node, shared_get_list()->head)
   {
@@ -119,7 +119,7 @@ report_cluster(struct Client *source_p)
   };
 
   dlink_node *node;
-  char buf[sizeof(flag_table) / sizeof(struct cluster_types)];
+  char buf[sizeof(flag_table) / sizeof(struct cluster_types) + 1];  /* +1 for 'C' */
 
   DLINK_FOREACH(node, cluster_get_list()->head)
   {

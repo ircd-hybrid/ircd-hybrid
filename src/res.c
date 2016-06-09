b5625ca5c0990d1fe4989587f813283d9fe14a88
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-06-09 15:51:59 +0000

    - res.c:send_res_msg(): remove extraneous parentheses
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@7592 82007160-df01-0410-b94d-b575c5fd34c7

--- src/res.c
+++ src/res.c
@@ -248,7 +248,7 @@ send_res_msg(const unsigned char *msg, int len, unsigned int rcount)
   for (unsigned int i = 0; i < max_queries; ++i)
   {
     if (sendto(ResolverFileDescriptor.fd, msg, len, 0,
-        (struct sockaddr*)&(irc_nsaddr_list[i]),
+        (struct sockaddr*)&irc_nsaddr_list[i],
         irc_nsaddr_list[i].ss_len) == len)
       ++sent;
   }

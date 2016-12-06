006094d7bc6ed9d46782aa4e3d44c8605a98c66d
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-12-06 20:24:38 +0000

    - m_kill.c: removed target's SID from the kill notice
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@7920 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_kill.c
+++ modules/core/m_kill.c
@@ -123,10 +123,9 @@ mo_kill(struct Client *source_p, int parc, char *parv[])
    * that have been around for ever, for no reason..
    */
   sendto_realops_flags(UMODE_SERVNOTICE, L_ALL, SEND_NOTICE,
-                       "Received KILL message for %s!%s@%s[%s/%s]. From %s Path: %s (%s)",
+                       "Received KILL message for %s!%s@%s[%s]. From %s Path: %s (%s)",
                        target_p->name, target_p->username, target_p->host,
                        target_p->servptr->name,
-                       target_p->servptr->id,
                        source_p->name, me.name, reason);
 
   ilog(LOG_TYPE_KILL, "KILL From %s For %s Path %s (%s)",
@@ -225,18 +224,17 @@ ms_kill(struct Client *source_p, int parc, char *parv[])
    */
   if (IsClient(source_p))  /* Send it normally */
     sendto_realops_flags(UMODE_SERVNOTICE, L_ALL, SEND_NOTICE,
-                         "Received KILL message for %s!%s@%s[%s/%s]. From %s Path: %s!%s!%s!%s %s",
+                         "Received KILL message for %s!%s@%s[%s]. From %s Path: %s!%s!%s!%s %s",
                          target_p->name, target_p->username, target_p->host,
                          target_p->servptr->name,
-                         target_p->servptr->id, source_p->name,
+                         source_p->name,
                          source_p->servptr->name, source_p->host, source_p->username,
                          source_p->name, reason);
   else
     sendto_realops_flags(UMODE_SKILL, L_ALL, SEND_NOTICE,
-                         "Received KILL message for %s!%s@%s[%s/%s]. From %s %s",
+                         "Received KILL message for %s!%s@%s[%s]. From %s %s",
                          target_p->name, target_p->username, target_p->host,
                          target_p->servptr->name,
-                         target_p->servptr->id,
                          source_p->name, reason);
 
   ilog(LOG_TYPE_KILL, "KILL From %s For %s Path %s %s",

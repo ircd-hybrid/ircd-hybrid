b9b3c8c6c31f3d9dbfc7bc9626f7405d1c39250b
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2016-12-31 13:57:24 +0000

    - Update copyright years
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@7925 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_part.c
+++ modules/core/m_part.c
@@ -1,7 +1,7 @@
 /*
  *  ircd-hybrid: an advanced, lightweight Internet Relay Chat Daemon (ircd)
  *
- *  Copyright (c) 1997-2016 ircd-hybrid development team
+ *  Copyright (c) 1997-2017 ircd-hybrid development team
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

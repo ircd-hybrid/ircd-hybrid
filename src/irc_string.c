dacee6caebc8b91a96f77bd591b4e8e5e6d6fdb7
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2017-04-09 11:27:54 +0000

    - irc_string.c: sync strlcpy() and strlcat() with latest versions from openbsd
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@8161 82007160-df01-0410-b94d-b575c5fd34c7

--- src/irc_string.c
+++ src/irc_string.c
@@ -161,97 +161,99 @@ libio_basename(const char *path)
   return s;
 }
 
+/*    $OpenBSD: strlcat.c,v 1.18 2016/10/16 17:37:39 dtucker Exp $    */
+/*    $OpenBSD: strlcpy.c,v 1.15 2016/10/16 17:37:39 dtucker Exp $    */
 /*
- * strlcat and strlcpy were ripped from openssh 2.5.1p2
- * They had the following Copyright info:
+ * Copyright (c) 1998, 2015 Todd C. Miller <Todd.Miller@courtesan.com>
  *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED `AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #ifndef HAVE_STRLCAT
+/*
+ * Appends src to string dst of size dsize (unlike strncat, dsize is the
+ * full size of dst, not space left).  At most dsize-1 characters
+ * will be copied.  Always NUL terminates (unless dsize <= strlen(dst)).
+ * Returns strlen(src) + MIN(dsize, strlen(initial dst)).
+ * If retval >= dsize, truncation occurred.
+ */
 size_t
-strlcat(char *dst, const char *src, size_t siz)
+strlcat(char *dst, const char *src, size_t dsize)
 {
-  char *d = dst;
-  const char *s = src;
-  size_t n = siz, dlen;
+  const char *odst = dst;
+  const char *osrc = src;
+  size_t n = dsize;
+  size_t dlen;
 
-  while (n-- != 0 && *d != '\0')
-    d++;
+  /* Find the end of dst and adjust bytes left but don't go past end. */
+  while (n-- != 0 && *dst != '\0')
+    dst++;
 
-  dlen = d - dst;
-  n    = siz - dlen;
+  dlen = dst - odst;
+  n = dsize - dlen;
 
-  if (n == 0)
-    return dlen + strlen(s);
+  if (n-- == 0)
+    return dlen + strlen(src);
 
-  while (*s != '\0')
+  while (*src != '\0')
   {
-    if (n != 1)
+    if (n != 0)
     {
-      *d++ = *s;
+      *dst++ = *src;
       n--;
     }
 
-    s++;
+    src++;
   }
 
-  *d = '\0';
-  return dlen + (s - src); /* count does not include NUL */
+  *dst = '\0';
+
+  return dlen + (src - osrc);  /* count does not include NUL */
 }
 #endif
 
 #ifndef HAVE_STRLCPY
+/*
+ * Copy string src to buffer dst of size dsize.  At most dsize-1
+ * chars will be copied.  Always NUL terminates (unless dsize == 0).
+ * Returns strlen(src); if retval >= dsize, truncation occurred.
+ */
 size_t
-strlcpy(char *dst, const char *src, size_t siz)
+strlcpy(char *dst, const char *src, size_t dsize)
 {
-  char *d = dst;
-  const char *s = src;
-  size_t n = siz;
+  const char *osrc = src;
+  size_t nleft = dsize;
 
-  /* Copy as many bytes as will fit */
-  if (n != 0 && --n != 0)
+  /* Copy as many bytes as will fit. */
+  if (nleft != 0)
   {
-    do
+    while (--nleft != 0)
     {
-      if ((*d++ = *s++) == 0)
+      if ((*dst++ = *src++) == '\0')
         break;
-    } while (--n != 0);
+    }
   }
 
-  /* Not enough room in dst, add NUL and traverse rest of src */
-  if (n == 0)
+  /* Not enough room in dst, add NUL and traverse rest of src. */
+  if (nleft == 0)
   {
-    if (siz != 0)
-      *d = '\0'; /* NUL-terminate dst */
-    while (*s++)
+    if (dsize != 0)
+      *dst = '\0';  /* NUL-terminate dst */
+
+    while (*src++)
       ;
   }
 
-  return s - src - 1; /* count does not include NUL */
+  return src - osrc - 1;  /* count does not include NUL */
 }
 #endif

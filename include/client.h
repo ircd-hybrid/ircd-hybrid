4bea45d57062bd949794441e4b296e1a430a9379
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2018-03-01 20:40:51 +0000

    - client.h: removed 'country_id' libgeoip leftover from Client struct
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@8333 82007160-df01-0410-b94d-b575c5fd34c7

--- include/client.h
+++ include/client.h
@@ -348,7 +348,6 @@ struct Connection
   dlink_list        invited;   /**< Chain of invite pointer blocks */
   struct irc_ssaddr ip;
   int               aftype;    /**< Makes life easier for DNS res in IPV6 */
-  int               country_id; /**< ID corresponding to a ISO 3166 country code */
 
   fde_t             fd;
 

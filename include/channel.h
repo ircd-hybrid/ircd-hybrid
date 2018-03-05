580b8f149d052a5d23b3b32bc714d6adea28b188
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2018-03-05 19:51:02 +0000

    - "JOIN 0" is now no longer supported
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@8366 82007160-df01-0410-b94d-b575c5fd34c7

--- include/channel.h
+++ include/channel.h
@@ -137,7 +137,6 @@ extern int is_banned(const struct Channel *, const struct Client *);
 extern int has_member_flags(const struct Membership *, const unsigned int);
 
 extern void channel_do_join(struct Client *, char *, char *);
-extern void channel_do_join_0(struct Client *);
 extern void channel_do_part(struct Client *, char *, const char *);
 extern void remove_ban(struct Ban *, dlink_list *);
 extern void channel_init(void);

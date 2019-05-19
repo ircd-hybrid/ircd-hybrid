2580e7c04ac83bdc0413208d76fbe3af618caabe
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2019-05-19 10:49:23 +0000

    - client.c, server.c: use monotonic time for server connection cycle
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@8979 82007160-df01-0410-b94d-b575c5fd34c7

--- src/server.c
+++ src/server.c
@@ -280,10 +280,10 @@ try_connections(void *unused)
      * made one successfull connection... [this algorithm is
      * a bit fuzzy... -- msa >;) ]
      */
-    if (conf->until > event_base->time.sec_real)
+    if (conf->until > event_base->time.sec_monotonic)
       continue;
 
-    conf->until = event_base->time.sec_real + conf->class->con_freq;
+    conf->until = event_base->time.sec_monotonic + conf->class->con_freq;
 
     /*
      * Found a CONNECT config with port specified, scan clients

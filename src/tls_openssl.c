1dafc9fd3b3af02be7ac4e931d17e731c5990384
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2020-01-12 13:19:21 +0000

    - tls_openssl.c:tls_new_cred(): despite being mentioned in the manual, feeding SSL_CTX_set_ciphersuites with an empty list doesn't work
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@9150 82007160-df01-0410-b94d-b575c5fd34c7

--- src/tls_openssl.c
+++ src/tls_openssl.c
@@ -179,12 +179,11 @@ tls_new_cred(void)
 
 #ifndef LIBRESSL_VERSION_NUMBER
   if (ConfigServerInfo.tls_cipher_suites == NULL)
-    /* Default to TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256 */
-    SSL_CTX_set_ciphersuites(ConfigServerInfo.tls_ctx.server_ctx, "");
+    SSL_CTX_set_ciphersuites(ConfigServerInfo.tls_ctx.server_ctx, "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256");
   else if (SSL_CTX_set_ciphersuites(ConfigServerInfo.tls_ctx.server_ctx, ConfigServerInfo.tls_cipher_suites) == 0)
   {
     ilog(LOG_TYPE_IRCD, "Ignoring serverinfo::tls_cipher_suites -- could not set supported cipher suite(s)");
-    SSL_CTX_set_ciphersuites(ConfigServerInfo.tls_ctx.server_ctx, "");
+    SSL_CTX_set_ciphersuites(ConfigServerInfo.tls_ctx.server_ctx, "TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_128_GCM_SHA256");
   }
 #endif
 

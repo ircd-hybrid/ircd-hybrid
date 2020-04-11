28a4a5547467149d6b67eda77eda40cd33ebb4e9
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2020-04-11 12:55:47 +0000

    - Remove strtok_r() compatibility code
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/trunk@9331 82007160-df01-0410-b94d-b575c5fd34c7

--- src/irc_string.c
+++ src/irc_string.c
@@ -63,42 +63,6 @@ stripws(char *txt)
   return txt;
 }
 
-/*
- * strtok_r - walk through a string of tokens, using a set of separators
- *   argv 9/90
- *
- */
-#ifndef HAVE_STRTOK_R
-char *
-strtok_r(char *str, const char *fs, char **save)
-{
-  char *pos = *save;  /* keep last position across calls */
-  char *tmp = NULL;
-
-  if (str)
-    pos = str;    /* new string scan */
-
-  while (pos && *pos && strchr(fs, *pos))
-    ++pos;        /* skip leading separators */
-
-  if (!pos || !*pos)
-    return pos = *save = NULL;   /* string contains only sep's */
-
-  tmp = pos;       /* now, keep position of the token */
-
-  while (*pos && strchr(fs, *pos) == NULL)
-    ++pos;       /* skip content of the token */
-
-  if (*pos)
-    *pos++ = '\0';    /* remove first sep after the token */
-  else
-    pos = NULL;    /* end of string */
-
-  *save = pos;
-  return tmp;
-}
-#endif /* !HAVE_STRTOK_R */
-
 /** Fill a vector of tokens from a delimited input list.
  * Empty tokens (when \a token occurs at the start or end of \a list,
  * or when \a token occurs adjacent to itself) are ignored.  When

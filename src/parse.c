3a3670290967ef2860dae4969016358931906883
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2020-05-15 20:43:52 +0000

    - parse.c:parse_handle_numeric(): unconfuse some extra smart looking condition
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@9401 82007160-df01-0410-b94d-b575c5fd34c7

--- src/parse.c
+++ src/parse.c
@@ -156,28 +156,10 @@ parse_remove_unknown(struct Client *client, const char *lsender, char *lbuffer)
 static void
 parse_handle_numeric(unsigned int numeric, struct Client *source, int parc, char *parv[])
 {
-  struct Client *target = NULL;
-  struct Channel *channel = NULL;
-
   /*
    * Avoid trash, we need it to come from a server and have a target
    */
   if (parc < 2 || !IsServer(source))
-    return;
-
-  /*
-   * Who should receive this message ? Will we do something with it ?
-   * Note that we use findUser functions, so the target can't be neither
-   * a server, nor a channel (?) nor a list of targets (?) .. u2.10
-   * should never generate numeric replies to non-users anyway
-   * Ahem... it can be a channel actually, csc bots use it :\ --Nem
-   */
-  if (IsChanPrefix(*parv[1]))
-    channel = hash_find_channel(parv[1]);
-  else
-    target = find_person(source, parv[1]);
-
-  if ((target == NULL || target->from == source->from) && channel == NULL)
     return;
 
   /*
@@ -192,8 +174,28 @@ parse_handle_numeric(unsigned int numeric, struct Client *source, int parc, char
   if (numeric < 100)
     numeric += 100;
 
-  if (target)
+  /*
+   * Who should receive this message ? Will we do something with it ?
+   * Note that we use findUser functions, so the target can't be neither
+   * a server, nor a channel (?) nor a list of targets (?) .. u2.10
+   * should never generate numeric replies to non-users anyway
+   * Ahem... it can be a channel actually, csc bots use it :\ --Nem
+   */
+  if (IsChanPrefix(*parv[1]))
   {
+    struct Channel *channel = hash_find_channel(parv[1]);
+    if (channel == NULL)
+      return;
+
+    sendto_channel_butone(source, source, channel, 0, "%u %s %s",
+                          numeric, channel->name, parv[2]);
+  }
+  else
+  {
+    struct Client *target = find_person(source, parv[1]);
+    if (target == NULL || target->from == source->from)
+      return;
+
     /* Fake it for server hiding, if it's our client */
     if ((ConfigServerHide.hide_servers || IsHidden(source)) && MyConnect(target) &&
         !HasUMode(target, UMODE_OPER))
@@ -201,9 +203,6 @@ parse_handle_numeric(unsigned int numeric, struct Client *source, int parc, char
     else
       sendto_one_numeric(target, source, numeric | SND_EXPLICIT, "%s", parv[2]);
   }
-  else
-    sendto_channel_butone(source, source, channel, 0, "%u %s %s",
-                          numeric, channel->name, parv[2]);
 }
 
 /* handle_command()

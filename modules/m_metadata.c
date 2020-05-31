d3978e63bdebbffcb4d67a14b8519f769572eda9
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2020-05-31 12:33:04 +0000

    - Add METADATA command handler for later use
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@9412 82007160-df01-0410-b94d-b575c5fd34c7

---
+++ modules/m_metadata.c
@@ -0,0 +1,90 @@
+/*
+ *  ircd-hybrid: an advanced, lightweight Internet Relay Chat Daemon (ircd)
+ *
+ *  Copyright (c) 2020-2020 ircd-hybrid development team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ *  USA
+ */
+
+/*! \file m_metadata.c
+ * \brief Includes required functions for processing the METADATA command.
+ * \version $Id$
+ */
+
+#include "stdinc.h"
+#include "list.h"
+#include "client.h"
+#include "ircd.h"
+#include "send.h"
+#include "parse.h"
+#include "modules.h"
+#include "irc_string.h"
+
+
+/*! \brief METADATA command handler
+ *
+ * \param source_p Pointer to allocated Client struct from which the message
+ *                 originally comes from.  This can be a local or remote client.
+ * \param parc     Integer holding the number of supplied arguments.
+ * \param parv     Argument vector where parv[0] .. parv[parc-1] are non-NULL
+ *                 pointers.
+ * \note Valid arguments for this command are:
+ *      - parv[0] = command
+ */
+static void
+ms_metadata(struct Client *source_p, int parc, char *parv[])
+{
+  if (!HasFlag(source_p, FLAGS_SERVICE) && !IsServer(source_p))
+    return;
+
+  if (parc == 4)
+    sendto_server(source_p, 0, 0, ":%s METADATA %s %s %s",
+                  source_p->id,
+                  parv[1], parv[2], parv[3]);
+  else
+    sendto_server(source_p, 0, 0, ":%s METADATA %s %s %s :%s",
+                  source_p->id,
+                  parv[1], parv[2], parv[3], parv[4]);
+}
+
+static struct Message metadata_msgtab =
+{
+  .cmd = "METADATA",
+  .handlers[UNREGISTERED_HANDLER] = { .handler = m_unregistered },
+  .handlers[CLIENT_HANDLER] = { .handler = m_ignore },
+  .handlers[SERVER_HANDLER] = { .handler = ms_metadata, .args_min = 4 },
+  .handlers[ENCAP_HANDLER] = { .handler = m_ignore },
+  .handlers[OPER_HANDLER] = { .handler = m_ignore }
+};
+
+static void
+module_init(void)
+{
+  mod_add_cmd(&metadata_msgtab);
+}
+
+static void
+module_exit(void)
+{
+  mod_del_cmd(&metadata_msgtab);
+}
+
+struct module module_entry =
+{
+  .version = "$Revision$",
+  .modinit = module_init,
+  .modexit = module_exit,
+};

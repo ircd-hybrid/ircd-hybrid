9bf0c6a163be56364c9570fdf1db439d7f5564d0
Valentin Lorentz <progval+git@progval.net>
2022-05-15 20:49:21 +0200

    Fix check of can_send()'s return value
    e7a57e3ae400bd5dd70257f5f16acb7621d94661 changed the constants,
    turning CAN_SEND_NONOP and CAN_SEND_OPV into positive values

--- src/channel.c
+++ src/channel.c
@@ -1133,7 +1133,7 @@ channel_part_one_client(struct Client *client, const char *name, const char *rea
   if (*reason && (!MyConnect(client) ||
       ((client->connection->created_monotonic +
         ConfigGeneral.anti_spam_exit_message_time) < event_base->time.sec_monotonic &&
-       can_send(channel, client, member, reason, false, &error) < 0)))
+       can_send(channel, client, member, reason, false, &error) != CAN_SEND_NO)))
   {
     sendto_server(client, 0, 0, ":%s PART %s :%s",
                   client->id, channel->name, reason);

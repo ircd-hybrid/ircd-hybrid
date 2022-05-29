ircd-hybrid/ircd-hybrid

commit 2875f577611a7d5ec6eda9f6f499e63ae13d01f1

- Implemented the owner/admin channel prefix modes. These are optional and can be enabled with the channel::enable_owner and channel::enable_admin configuration directives
git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@10042 82007160-df01-0410-b94d-b575c5fd34c7

src/channel.c
@@ -153,28 +153,17 @@ channel_demote_members(struct Channel *channel, const struct Client *client)
   char *mbuf = modebuf;
   char *pbuf = parabuf;
   unsigned int pargs = 0;
-  struct member_status
-  {
-    unsigned char prefix;
-    unsigned int flag;
-  } table[] =
-  {
-    { 'o', CHFL_CHANOP },
-    { 'h', CHFL_HALFOP },
-    { 'v', CHFL_VOICE  },
-    { '\0', 0 }
-  };
 
   DLINK_FOREACH(node, channel->members.head)
   {
     struct ChannelMember *member = node->data;
 
-    for (const struct member_status *status = table; status->flag; ++status)
+    for (const struct chan_mode *tab = cflag_tab; tab->letter; ++tab)
     {
-      if (member->flags & status->flag)
+      if (member_has_flags(member, tab->flag) == true)
       {
-        member->flags &= ~status->flag;
-        *mbuf++ = status->prefix;
+        member->flags &= ~tab->flag;
+        *mbuf++ = tab->letter;
         pbuf += snprintf(pbuf, sizeof(parabuf) - (pbuf - parabuf), "%s ", member->client->name);
 
         if (++pargs >= MAXMODEPARAMS)
@@ -541,24 +530,12 @@ member_get_prefix(const struct ChannelMember *member, bool combine)
   static char buf[CMEMBER_STATUS_FLAGS_LEN + 1];  /* +1 for \0 */
   char *bufptr = buf;
 
-  if (member->flags & CHFL_CHANOP)
-  {
-    if (combine == false)
-      return "@";
-    *bufptr++ = '@';
-  }
+  for (const struct chan_mode *tab = cflag_tab; tab->letter; ++tab)
+    if (member_has_flags(member, tab->flag) == true)
+      if (*bufptr++ = tab->prefix, combine == false)
+        break;
 
-  if (member->flags & CHFL_HALFOP)
-  {
-    if (combine == false)
-      return "%";
-    *bufptr++ = '%';
-  }
-
-  if (member->flags & CHFL_VOICE)
-    *bufptr++ = '+';
   *bufptr = '\0';
-
   return buf;
 }
 
@@ -567,28 +544,33 @@ member_get_prefix_len(const struct ChannelMember *member, bool combine)
 {
   size_t len = 0;
 
-  if (member->flags & CHFL_CHANOP)
-  {
-    if (combine == false)
-      return 1;
-    ++len;
-  }
-
-  if (member->flags & CHFL_HALFOP)
-  {
-    if (combine == false)
-      return 1;
-    ++len;
-  }
-
-  if (member->flags & CHFL_VOICE)
-  {
-    if (combine == false)
-      return 1;
-    ++len;
-  }
+  for (const struct chan_mode *tab = cflag_tab; tab->letter; ++tab)
+    if (member_has_flags(member, tab->flag) == true)
+      if (++len, combine == false)
+        break;
 
   return len;
+}
+
+int
+member_highest_rank(const struct ChannelMember *member)
+{
+  if (member == NULL)
+    return CHACCESS_NOTONCHAN;
+
+  if (member_has_flags(member, CHFL_CHANOWNER) == true)
+    return CHACCESS_CHANOWNER;
+
+  if (member_has_flags(member, CHFL_CHANADMIN) == true)
+    return CHACCESS_CHANADMIN;
+
+  if (member_has_flags(member, CHFL_CHANOP) == true)
+    return CHACCESS_CHANOP;
+
+  if (member_has_flags(member, CHFL_HALFOP) == true)
+    return CHACCESS_HALFOP;
+
+  return CHACCESS_PEON;
 }
 
 /*!
@@ -828,7 +810,7 @@ can_send(struct Channel *channel, struct Client *client,
   }
 
   if (member || (member = member_find_link(client, channel)))
-    if (member->flags & (CHFL_CHANOP | CHFL_HALFOP | CHFL_VOICE))
+    if (member_highest_rank(member) > CHACCESS_PEON)
       return CAN_SEND_OPV;
 
   if (member == NULL && HasCMode(channel, MODE_NOPRIVMSGS))

doc/reference.conf
@@ -891,6 +891,24 @@ channel {
 	enable_extbans = no;
 
 	/*
+	 * enable_owner: if set to 'yes', support for the owner (+q) channel
+	 * prefix mode is enabled.
+	 * This option must be the same on all servers participating in the
+	 * topology. Servers attempting to connect with a different setting
+	 * will be rejected by this server.
+	 *
+	 * Default is 'no'. This cannot be changed at runtime.
+	 */
+	enable_owner = no;
+
+	/*
+	 * enable_admin: if set to 'yes', support for the admin (+a) channel
+	 * prefix mode is enabled.
+	 * Same notes as for 'enable_owner' apply here, too.
+	 */
+	enable_admin = no;
+
+	/*
 	 * disable_fake_channels: this option, if set to 'yes', will
 	 * disallow clients from creating or joining channels that have one
 	 * of the following ASCII characters in their name:

include/channel.h
@@ -149,6 +149,7 @@ extern void channel_set_topic(struct Channel *, const char *, const char *, uint
 
 extern const char *member_get_prefix(const struct ChannelMember *, bool);
 extern size_t member_get_prefix_len(const struct ChannelMember *, bool);
+extern int member_highest_rank(const struct ChannelMember *);
 
 extern struct Channel *channel_make(const char *);
 extern struct ChannelMember *member_find_link(const struct Client *, const struct Channel *);

include/channel_mode.h
@@ -28,7 +28,7 @@
 #define INCLUDED_channel_mode_h
 
 
-#define CMEMBER_STATUS_FLAGS "@%+"
+#define CMEMBER_STATUS_FLAGS "~&@%+"
 enum { CMEMBER_STATUS_FLAGS_LEN = sizeof(CMEMBER_STATUS_FLAGS) - 1 };
 
 enum { MODEBUFLEN = 200 };
@@ -47,8 +47,11 @@ enum
 {
   CHACCESS_NOTONCHAN = -1,
   CHACCESS_PEON,
+  CHACCESS_VOICE,
   CHACCESS_HALFOP,
   CHACCESS_CHANOP,
+  CHACCESS_CHANADMIN,
+  CHACCESS_CHANOWNER,
   CHACCESS_REMOTE
 };
 
@@ -81,16 +84,18 @@ enum mode_class
 /* Channel related flags */
 enum
 {
-  CHFL_CHANOP       = 1 << 0,  /* Channel operator   */
-  CHFL_HALFOP       = 1 << 1,  /* Channel half op    */
-  CHFL_VOICE        = 1 << 2,  /* the power to speak */
-  CHFL_BAN          = 1 << 3,  /* ban channel flag */
-  CHFL_EXCEPTION    = 1 << 4,  /* exception to ban channel flag */
-  CHFL_INVEX        = 1 << 5,
+  CHFL_CHANOWNER    = 1 <<  0,  /**< Channel owner (~) */
+  CHFL_CHANADMIN    = 1 <<  1,  /**< Channel admin (&) */
+  CHFL_CHANOP       = 1 <<  2,  /**< Channel operator (@) */
+  CHFL_HALFOP       = 1 <<  3,  /**< Channel half-op (%) */
+  CHFL_VOICE        = 1 <<  4,  /**< Channel voice (+) */
+  CHFL_BAN          = 1 <<  5,  /* ban channel flag */
+  CHFL_EXCEPTION    = 1 <<  6,  /* exception to ban channel flag */
+  CHFL_INVEX        = 1 <<  7,
   /* Cache flags for silence on ban */
-  CHFL_BAN_CHECKED  = 1 << 6,
-  CHFL_BAN_SILENCED = 1 << 7,
-  CHFL_MUTE_CHECKED = 1 << 8
+  CHFL_BAN_CHECKED  = 1 <<  8,
+  CHFL_BAN_SILENCED = 1 <<  9,
+  CHFL_MUTE_CHECKED = 1 << 10,
 };
 
 /* channel modes ONLY */
@@ -98,8 +103,8 @@ enum
 {
   MODE_PRIVATE      = 1 <<  0,  /**< */
   MODE_SECRET       = 1 <<  1,  /**< Channel does not show up on NAMES or LIST */
-  MODE_MODERATED    = 1 <<  2,  /**< Users without +v/+h/+o cannot send text to the channel */
-  MODE_TOPICLIMIT   = 1 <<  3,  /**< Only chanops can change the topic */
+  MODE_MODERATED    = 1 <<  2,  /**< Users without +v or higher cannot send text to the channel */
+  MODE_TOPICLIMIT   = 1 <<  3,  /**< Only chanops or higher can change the topic */
   MODE_INVITEONLY   = 1 <<  4,  /**< Only invited users may join this channel */
   MODE_NOPRIVMSGS   = 1 <<  5,  /**< Users must be in the channel to send text to it */
   MODE_SECUREONLY   = 1 <<  6,  /**< Prevents anyone who isn't connected via TLS from joining the channel */
@@ -136,6 +141,7 @@ struct ChModeChange
 struct chan_mode
 {
   unsigned char letter;
+  unsigned char prefix;
   unsigned int mode;
   unsigned int flag;
   enum mode_class class;
@@ -149,6 +155,7 @@ struct chan_mode
 
 extern const struct chan_mode *cmode_map[];
 extern const struct chan_mode  cmode_tab[];
+extern const struct chan_mode  cflag_tab[];
 extern char cmode_rpl04[][256];
 extern char cmode_class[][256];
 

include/conf.h
@@ -213,6 +213,8 @@ struct config_general_entry
 struct config_channel_entry
 {
   unsigned int enable_extbans;
+  unsigned int enable_owner;
+  unsigned int enable_admin;
   unsigned int disable_fake_channels;
   unsigned int invite_client_count;
   unsigned int invite_client_time;

include/send.h
@@ -63,9 +63,9 @@ extern void send_queued_write(struct Client *);
 extern void sendto_one(struct Client *, const char *, ...) AFP(2,3);
 extern void sendto_one_numeric(struct Client *, const struct Client *, enum irc_numerics, ...);
 extern void sendto_one_notice(struct Client *, const struct Client *, const char *, ...) AFP(3,4);
-extern void sendto_channel_butone(struct Client *, const struct Client *, struct Channel *, unsigned int, const char *, ...) AFP(5,6);
+extern void sendto_channel_butone(struct Client *, const struct Client *, struct Channel *, int, const char *, ...) AFP(5,6);
 extern void sendto_common_channels_local(struct Client *, bool, unsigned int, unsigned int, const char *, ...) AFP(5,6);
-extern void sendto_channel_local(const struct Client *, struct Channel *, unsigned int, unsigned int, unsigned int, const char *, ...) AFP(6,7);
+extern void sendto_channel_local(const struct Client *, struct Channel *, int, unsigned int, unsigned int, const char *, ...) AFP(6,7);
 extern void sendto_server(const struct Client *, const unsigned int, const unsigned int, const char *, ...) AFP(4,5);
 extern void sendto_match_butone(const struct Client *, const struct Client *, const char *, int, const char *, ...) AFP(5,6);
 extern void sendto_match_servs(const struct Client *, const char *, unsigned int, const char *, ...) AFP(4,5);

include/server_capab.h
@@ -39,7 +39,10 @@ enum
   CAPAB_TBURST  = 1 <<  6,  /**< Supports TBURST */
   CAPAB_DLN     = 1 <<  7,  /**< Can do DLINE message */
   CAPAB_UNDLN   = 1 <<  8,  /**< Can do UNDLINE message */
-  CAPAB_RHOST   = 1 <<  9  /**< Can do extended realhost UID messages */
+  CAPAB_RHOST   = 1 <<  9,  /**< Can do extended realhost UID messages */
+  CAPAB_QOP     = 1 << 10,  /**< Server supports the owner (~) channel prefix mode */
+  CAPAB_AOP     = 1 << 11,  /**< Server supports the admin (&) channel prefix mode */
+  CAPAB_HOP     = 1 << 12,  /**< Server supports the half-op (%) channel prefix mode */
 };
 
 /*
@@ -55,11 +58,12 @@ struct Capability
   dlink_node node;  /**< List node; linked into capab_list */
   char *name;  /**< Name of capability */
   unsigned int cap;  /**< Mask value */
+  bool active;  /**< Capability is active on this server and advertized to others */
 };
 
 extern void capab_init(void);
-extern void capab_add(const char *, unsigned int);
+extern void capab_add(const char *, unsigned int, bool);
 extern void capab_del(const char *);
 extern unsigned int capab_find(const char *);
-extern const char *capab_get(const void *);
+extern const char *capab_get(const void *, bool);
 #endif  /* INCLUDED_server_capab_h */

modules/core/m_kick.c
@@ -71,7 +71,7 @@ m_kick(struct Client *source_p, int parc, char *parv[])
     return;
   }
 
-  if (member_has_flags(member_source, CHFL_CHANOP | CHFL_HALFOP) == false)
+  if (member_highest_rank(member_source) < CHACCESS_HALFOP)
   {
     sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, channel->name);
     return;
@@ -88,13 +88,10 @@ m_kick(struct Client *source_p, int parc, char *parv[])
     return;
   }
 
-  if (member_has_flags(member_source, CHFL_CHANOP) == false)
+  if (member_highest_rank(member_source) < member_highest_rank(member_target))
   {
-    if (member_has_flags(member_target, CHFL_CHANOP | CHFL_HALFOP) == true)
-    {
-      sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, channel->name);
-      return;
-    }
+    sendto_one_numeric(source_p, &me, ERR_CHANOPRIVSNEEDED, channel->name);
+    return;
   }
 
   char reason[KICKLEN + 1];

commit c2a4746e00563b3df0b372efffd152622ddafc68

Fix +v users being unable to send messages to +m channels
Bug introduced in 2875f577611a7d5ec6eda9f6f499e63ae13d01f1.

src/channel.c
@@ -570,6 +570,9 @@ member_highest_rank(const struct ChannelMember *member)
   if (member_has_flags(member, CHFL_HALFOP) == true)
     return CHACCESS_HALFOP;
 
+  if (member_has_flags(member, CHFL_VOICE) == true)
+    return CHACCESS_VOICE;
+
   return CHACCESS_PEON;
 }
 
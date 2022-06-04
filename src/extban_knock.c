c5fcbbf3daf4a9c173e906ee973083a08ea7ebeb
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2022-06-04 16:58:59 +0000

    - add extban_knock.c;  autoreconf
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@10056 82007160-df01-0410-b94d-b575c5fd34c7

---
+++ src/extban_knock.c
@@ -0,0 +1,41 @@
+/*
+ *  ircd-hybrid: an advanced, lightweight Internet Relay Chat Daemon (ircd)
+ *
+ *  Copyright (c) 2015-2016 plexus development team
+ *  Copyright (c) 2019-2022 ircd-hybrid development team
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
+ *  USA
+ */
+
+/*! \file extban_knock.c
+ * \brief Implements $K extban which bans users from using the KNOCK command.
+ * \version $Id: extban_mute.c 10054 2022-06-04 16:41:14Z michael $
+ */
+
+#include "stdinc.h"
+#include "list.h"
+#include "channel.h"
+#include "channel_mode.h"
+#include "client.h"
+#include "extban.h"
+
+
+struct Extban extban_knock =
+{
+  .character = 'K',
+  .type = EXTBAN_ACTING,
+  .types = CHFL_BAN | CHFL_EXCEPTION
+};

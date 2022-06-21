107885f583b57788dcd8d11b6f85c89024a31dfe
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2022-06-21 08:38:30 +0000

    - m_join.c:ms_join(): readability improvements
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid/branches/8.2.x@10131 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/core/m_join.c
+++ modules/core/m_join.c
@@ -183,6 +183,9 @@ ms_join(struct Client *source_p, int parc, char *parv[])
     return;
   }
 
+  uintmax_t newts = strtoumax(parv[1], NULL, 10);
+  uintmax_t oldts = 0;
+
   struct Channel *channel = hash_find_channel(parv[2]);
   if (channel == NULL)
   {
@@ -194,18 +197,12 @@ ms_join(struct Client *source_p, int parc, char *parv[])
 
     isnew = true;
     channel = channel_make(parv[2]);
-  }
-
-  uintmax_t newts = strtoumax(parv[1], NULL, 10);
-  uintmax_t oldts = channel->creation_time;
-
-  if (isnew == true)
     channel->creation_time = newts;
-  else if (newts == oldts)
-    ;
-  else if (newts < oldts)
+  }
+  else if (newts < channel->creation_time)
   {
     keep_our_modes = false;
+    oldts = channel->creation_time;
     channel->creation_time = newts;
   }
 

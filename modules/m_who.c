752e2ac760367c19c730121a3562b86035912669
miwob <wobst.michael@web.de>
2022-08-27 13:38:36 +0200

    - m_who.c: fixed broken logic where `WHO mask` wouldn't report anything as pointed out by @progval in gh issue #39

--- modules/m_who.c
+++ modules/m_who.c
@@ -564,13 +564,15 @@ m_who(struct Client *source_p, int parc, char *parv[])
   {
     const struct Client *target_p = find_person(source_p, mask);
     if (target_p)
+    {
       if (!(who->bitsel & WHOSELECT_OPER) ||
         (HasUMode(target_p, UMODE_OPER) &&
           (!HasUMode(target_p, UMODE_HIDDEN) || HasUMode(source_p, UMODE_OPER))))
         who_send(source_p, target_p, NULL, who);
 
-    sendto_one_numeric(source_p, &me, RPL_ENDOFWHO, mask);
-    return;
+      sendto_one_numeric(source_p, &me, RPL_ENDOFWHO, mask);
+      return;
+    }
   }
 
   collapse(mask);

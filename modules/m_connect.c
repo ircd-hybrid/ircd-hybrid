372a63f4715771f33cf626503bc38f5715502a2f
miwob <wobst.michael@web.de>
2024-01-26 13:27:58 +0000

    - m_connect.c: deduplicate

--- modules/m_connect.c
+++ modules/m_connect.c
@@ -37,6 +37,72 @@
 #include "modules.h"
 
 
+static void
+do_connect(struct Client *source_p, const char *name)
+{
+  assert(IsClient(source_p));
+
+  /*
+   * Try to find the name. If it fails, notify and bail.
+   */
+  struct MaskItem *conf = connect_find(name, match);
+  if (conf == NULL)
+  {
+    sendto_one_notice(source_p, &me, ":Connect: Server %s not listed in configuration file", name);
+    return;
+  }
+
+  const struct Client *target_p = hash_find_server(conf->name);
+  if (target_p)
+  {
+    sendto_one_notice(source_p, &me, ":Connect: Server %s already exists from %s",
+                      target_p->name, target_p->from->name);
+    return;
+  }
+
+  if (find_servconn_in_progress(conf->name))
+  {
+    sendto_one_notice(source_p, &me, ":Connect: a connection to %s is already in progress",
+                      conf->name);
+    return;
+  }
+
+  /* Notify all operators about connect requests. */
+  static const char *const type[] =
+  {
+    "Remote", "Local"
+  };
+  const char *const type_p = type[MyConnect(source_p) != 0];
+
+  sendto_realops_flags(UMODE_SERVNOTICE, L_ALL, SEND_GLOBAL, "from %s: %s CONNECT %s %u from %s",
+                       me.name, type_p, name, conf->port, get_oper_name(source_p));
+  sendto_server(NULL, 0, 0, ":%s GLOBOPS :%s CONNECT %s %u from %s",
+                me.id, type_p, name, conf->port, get_oper_name(source_p));
+
+  ilog(LOG_TYPE_IRCD, "%s CONNECT %s %u from %s",
+       type_p, name, conf->port, get_oper_name(source_p));
+
+  /*
+   * At this point we should be calling connect_server with a valid
+   * connect{} block and a valid port in the connect{} block.
+   */
+  if (server_connect(conf, source_p) == false)
+    sendto_one_notice(source_p, &me, ":*** Couldn't connect to %s.%u",
+                      conf->name, conf->port);
+  else if (MyConnect(source_p) &&
+           (ConfigServerHide.hide_server_ips == 0 && HasUMode(source_p, UMODE_ADMIN)))
+    sendto_one_notice(source_p, &me, ":*** Connecting to %s[%s].%u",
+                      conf->name, conf->host, conf->port);
+  else
+    sendto_one_notice(source_p, &me, ":*** Connecting to %s.%u",
+                      conf->name, conf->port);
+
+  /*
+   * Client is either connecting with all the data it needs or has been
+   * destroyed.
+   */
+}
+
 /*! \brief CONNECT command handler
  *
  * \param source_p Pointer to allocated Client struct from which the message
@@ -53,8 +119,6 @@
 static void
 mo_connect(struct Client *source_p, int parc, char *parv[])
 {
-  const char *const name = parv[1];
-
   if (!EmptyString(parv[3]))
   {
     if (!HasOFlag(source_p, OPER_FLAG_CONNECT_REMOTE))
@@ -73,55 +137,8 @@ mo_connect(struct Client *source_p, int parc, char *parv[])
     return;
   }
 
-  /*
-   * Try to find the name. If it fails, notify and bail.
-   */
-  struct MaskItem *conf = connect_find(name, match);
-  if (conf == NULL)
-  {
-    sendto_one_notice(source_p, &me, ":Connect: Server %s not listed in configuration file", name);
-    return;
-  }
-
-  const struct Client *target_p = hash_find_server(conf->name);
-  if (target_p)
-  {
-    sendto_one_notice(source_p, &me, ":Connect: Server %s already exists from %s",
-                      target_p->name, target_p->from->name);
-    return;
-  }
-
-  if (find_servconn_in_progress(conf->name))
-  {
-    sendto_one_notice(source_p, &me, ":Connect: a connection to %s is already in progress",
-                      conf->name);
-    return;
-  }
-
-  ilog(LOG_TYPE_IRCD, "CONNECT %s %u from %s",
-       name, conf->port, get_oper_name(source_p));
-
-  /*
-   * At this point we should be calling connect_server with a valid
-   * connect{} block and a valid port in the connect{} block
-   */
-  if (server_connect(conf, source_p) == true)
-  {
-    if (ConfigServerHide.hide_server_ips == 0 && HasUMode(source_p, UMODE_ADMIN))
-      sendto_one_notice(source_p, &me, ":*** Connecting to %s[%s].%u",
-                        conf->name, conf->host, conf->port);
-    else
-      sendto_one_notice(source_p, &me, ":*** Connecting to %s.%u",
-                        conf->name, conf->port);
-  }
-  else
-    sendto_one_notice(source_p, &me, ":*** Couldn't connect to %s.%u",
-                      conf->name, conf->port);
-
-  /*
-   * Client is either connecting with all the data it needs or has been
-   * destroyed
-   */
+  const char *const name = parv[1];
+  do_connect(source_p, name);
 }
 
 /*! \brief CONNECT command handler
@@ -140,62 +157,11 @@ mo_connect(struct Client *source_p, int parc, char *parv[])
 static void
 ms_connect(struct Client *source_p, int parc, char *parv[])
 {
-  const char *const name = parv[1];
-
   if (server_hunt(source_p, ":%s CONNECT %s %s :%s", 3, parv)->ret != HUNTED_ISME)
     return;
 
-  /*
-   * Try to find the name. If it fails, notify and bail.
-   */
-  struct MaskItem *conf = connect_find(name, match);
-  if (conf == NULL)
-  {
-    sendto_one_notice(source_p, &me, ":Connect: Server %s not listed in configuration file", name);
-    return;
-  }
-
-  const struct Client *target_p = hash_find_server(conf->name);
-  if (target_p)
-  {
-    sendto_one_notice(source_p, &me, ":Connect: Server %s already exists from %s",
-                      target_p->name, target_p->from->name);
-    return;
-  }
-
-  if (find_servconn_in_progress(conf->name))
-  {
-    sendto_one_notice(source_p, &me, ":Connect: a connection to %s is already in progress",
-                      conf->name);
-    return;
-  }
-
-  /*
-   * Notify all operators about remote connect requests
-   */
-  sendto_realops_flags(UMODE_SERVNOTICE, L_ALL, SEND_GLOBAL, "from %s: Remote CONNECT %s %u from %s",
-                       me.name, name, conf->port, get_oper_name(source_p));
-  sendto_server(NULL, 0, 0, ":%s GLOBOPS :Remote CONNECT %s %u from %s",
-                me.id, name, conf->port, get_oper_name(source_p));
-
-  ilog(LOG_TYPE_IRCD, "Remote CONNECT %s %u from %s",
-       name, conf->port, get_oper_name(source_p));
-
-  /*
-   * At this point we should be calling connect_server with a valid
-   * connect{} block and a valid port in the connect{} block
-   */
-  if (server_connect(conf, source_p) == true)
-    sendto_one_notice(source_p, &me, ":*** Connecting to %s.%u",
-                      conf->name, conf->port);
-  else
-    sendto_one_notice(source_p, &me, ":*** Couldn't connect to %s.%u",
-                      conf->name, conf->port);
-
-  /*
-   * Client is either connecting with all the data it needs or has been
-   * destroyed
-   */
+  const char *const name = parv[1];
+  do_connect(source_p, name);
 }
 
 static struct Message connect_msgtab =

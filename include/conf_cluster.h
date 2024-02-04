22fe2117e790dd7ad968e3aae4b5d5e8441749d0
miwob <wobst.michael@web.de>
2024-02-04 21:18:44 +0000

    - conf_cluster.h: add proper documentation

--- include/conf_cluster.h
+++ include/conf_cluster.h
@@ -19,33 +19,33 @@
  *  USA
  */
 
-/*! \file conf_cluster.h
- * \brief Implements cluster {} block configuration management.
+/**
+ * @file conf_cluster.h
+ * @brief Implements cluster {} block management for sharing IRC commands between servers.
  */
 
 #ifndef INCLUDED_conf_cluster_h
 #define INCLUDED_conf_cluster_h
 
+/**
+ * @brief Configuration options for clustering.
+ * @details Defines constants representing different configuration options
+ * for clustering. Each option corresponds to a specific type of IRC command
+ * that can be shared among clustered servers.
+ */
 enum
 {
-  CLUSTER_KLINE   = 1 << 0,
-  CLUSTER_UNKLINE = 1 << 1,
-  CLUSTER_XLINE   = 1 << 2,
-  CLUSTER_UNXLINE = 1 << 3,
-  CLUSTER_RESV    = 1 << 4,
-  CLUSTER_UNRESV  = 1 << 5,
-  CLUSTER_LOCOPS  = 1 << 6,
-  CLUSTER_DLINE   = 1 << 7,
-  CLUSTER_UNDLINE = 1 << 8,
-  CLUSTER_REHASH  = 1 << 9,
-  CLUSTER_ALL     = UINT_MAX
-};
-
-struct ClusterItem
-{
-  dlink_node node;
-  char *server;
-  unsigned int type;
+  CLUSTER_KLINE   = 1 << 0, /*!< Share KLINE commands */
+  CLUSTER_UNKLINE = 1 << 1, /*!< Share UNKLINE commands */
+  CLUSTER_XLINE   = 1 << 2, /*!< Share XLINE commands */
+  CLUSTER_UNXLINE = 1 << 3, /*!< Share UNXLINE commands */
+  CLUSTER_RESV    = 1 << 4, /*!< Share RESV commands */
+  CLUSTER_UNRESV  = 1 << 5, /*!< Share UNRESV commands */
+  CLUSTER_LOCOPS  = 1 << 6, /*!< Share LOCOPS commands */
+  CLUSTER_DLINE   = 1 << 7, /*!< Share DLINE commands */
+  CLUSTER_UNDLINE = 1 << 8, /*!< Share UNDLINE commands */
+  CLUSTER_REHASH  = 1 << 9, /*!< Share REHASH commands */
+  CLUSTER_ALL     = UINT_MAX /*!< Share all supported commands (default) */
 };
 
 extern const dlink_list *cluster_get_list(void);

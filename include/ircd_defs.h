da5d117569d1140eaa0776b6743d53de0be3be12
miwob <wobst.michael@web.de>
2024-02-08 23:46:06 +0000

    - ircd_defs.h: add proper documentation

--- include/ircd_defs.h
+++ include/ircd_defs.h
@@ -21,57 +21,56 @@
 
 /*! \file ircd_defs.h
  * \brief A header for ircd global definitions.
+ *
+ * This header file defines various global constants and structures used
+ * throughout the ircd. It includes size definitions for record entries,
+ * adhering to IRC and RFC standards. Modification of the defined constants
+ * is discouraged without careful consideration, as they are fundamental
+ * to the functioning of the ircd.
  */
 
-/* ircd_defs.h - Global size definitions for record entries used
- * througout ircd. Please think 3 times before adding anything to this
- * file.
- */
 #ifndef INCLUDED_ircd_defs_h
 #define INCLUDED_ircd_defs_h
 #include "stdinc.h"
 
-/* Right out of the RFC */
-#define IRCD_BUFSIZE    512     /* WARNING: *DONT* CHANGE THIS!!!!         */
-#define HOSTLEN          63     /* Length of hostname. Updated to comply
-                                   with RFC 1123 */
-/*
- * NICKLEN: maximum size of a nick name that your server will be able to handle
- * when receiving nicks from remote servers. Do not change this; if you want to
- * change the maximum allowed nickname length, you can do this in the ircd.conf
+/**
+ * \defgroup ircd_constants IRCd Constants
+ * @{
  */
-#define NICKLEN         30
-#define USERLEN         10
-#define ACCOUNTLEN      NICKLEN
-#define PORTNAMELEN 6  /* ":31337" */
 
-#define HOSTIPLEN       45 /* sizeof("ffff:ffff:ffff:ffff:ffff:ffff:255.255.255.255") */
-#define PASSWDLEN       128
-#define IDLEN           12 /* this is the maximum length, not the actual
-                              generated length; DO NOT CHANGE! */
-#define REALLEN         50
-#define CHANNELLEN      50
+#define IRCD_BUFSIZE    512  /**< Maximum size of IRC message buffer. WARNING: *DON'T* CHANGE THIS!!!! */
+#define HOSTLEN         63  /**< Length of hostname. Updated to comply with RFC 1123. */
+#define NICKLEN         30  /**< Maximum size of a nickname that the server will handle. Do not change; configurable in ircd.conf. */
+#define USERLEN         10  /**< Maximum size of a username. */
+#define ACCOUNTLEN      NICKLEN  /**< Maximum size of an account name, equal to NICKLEN. */
+#define PORTNAMELEN     6  /**< Length of port name, e.g., ":31337". */
+#define HOSTIPLEN       45  /**< Maximum length of a string representation of an IP address. */
 
-/*
- * TOPICLEN: maximum size of a topic that your server will be able to handle
- * when receiving topics from remote servers. Do not change this; if you want to
- * change the maximum allowed topic length, you can do this in the ircd.conf
- */
-#define TOPICLEN        300
-#define REASONLEN       260
-#define KICKLEN         180
-#define AWAYLEN         180
-#define KEYLEN          23
+#define PASSWDLEN       128  /**< Maximum length of a password. */
+#define IDLEN           12  /**< Maximum length of an identifier. Do not change; not the actual generated length. */
+#define REALLEN         50  /**< Maximum size of a real name (GECOS). */
+#define CHANNELLEN      50  /**< Maximum size of a channel name. */
+#define TOPICLEN        300  /**< Maximum size of a channel topic. Do not change; configurable in ircd.conf. */
+#define REASONLEN       260  /**< Maximum size of a kick or ban reason. */
+#define KICKLEN         180  /**< Maximum size of a kick message. */
+#define AWAYLEN         180  /**< Maximum size of an away message. */
+#define KEYLEN          23  /**< Maximum size of a channel key. */
 
-#define MAX_DATE_STRING 32      /* maximum string length for a date string */
-#define LOWEST_SAFE_FD  3  /* skip stdin, stdout, and stderr */
+#define MAX_DATE_STRING 32  /**< Maximum string length for a date string. */
+#define LOWEST_SAFE_FD  3  /**< Lowest safe file descriptor number; skipping stdin, stdout, and stderr. */
 
-/* This is to get around the fact that some implementations have ss_len and
- * others do not
+/** @} */
+
+/**
+ * @struct irc_ssaddr
+ * @brief Structure to handle sockaddr_storage with compatibility for different implementations.
+ *
+ * This structure provides a consistent interface for sockaddr_storage with compatibility
+ * for different implementations that may or may not include the ss_len member.
  */
 struct irc_ssaddr
 {
-  struct sockaddr_storage ss;
-  unsigned char   ss_len;
+  struct sockaddr_storage ss;  /**< Underlying sockaddr_storage structure. */
+  unsigned char ss_len;  /**< Length of the sockaddr_storage. */
 };
 #endif /* INCLUDED_ircd_defs_h */

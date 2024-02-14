ircd-hybrid/ircd-hybrid

commit a5401152ad2c4ffad9b10da86bbcca6f3e7b3472

- add dlinkFindCmp() and make use of it in some places

src/hash.c
@@ -513,15 +513,10 @@ free_list_task(struct Client *client)
 static bool
 list_allow_channel(const char *name, const struct ListTask *lt)
 {
-  dlink_node *node;
 
-  DLINK_FOREACH(node, lt->show_mask.head)
-    if (match(node->data, name) != 0)
-      return false;
-
-  DLINK_FOREACH(node, lt->hide_mask.head)
-    if (match(node->data, name) == 0)
-      return false;
+  if (dlinkFindCmp(&lt->show_mask, name, match) == NULL ||
+      dlinkFindCmp(&lt->hide_mask, name, match))
+    return false;
 
   return true;
 }

include/list.h
@@ -107,6 +107,7 @@ extern void dlinkDelete(dlink_node *, dlink_list *);
 extern void dlinkMoveList(dlink_list *, dlink_list *);
 extern void dlink_move_node(dlink_node *, dlink_list *, dlink_list *);
 extern dlink_node *dlinkFind(dlink_list *, const void *);
+extern dlink_node *dlinkFindCmp(const dlink_list *, const void *, int (*)(const char *, const char *));
 extern dlink_node *dlinkFindDelete(dlink_list *, void *);
 extern dlink_node *make_dlink_node(void);
 #endif  /* INCLUDED_list_h */

modules/core/m_server.c
@@ -598,28 +598,10 @@ ms_sid(struct Client *source_p, int parc, char *parv[])
    * See if the newly found server is behind a guaranteed
    * leaf. If so, close the link.
    */
-  dlink_node *node;
-  bool hlined = false;
-  bool llined = false;
   const struct MaskItem *conf = source_p->from->connection->confs.head->data;
+  bool hlined = dlinkFindCmp(&conf->hub_list , parv[1], match) != NULL;
+  bool llined = dlinkFindCmp(&conf->leaf_list, parv[1], match) != NULL;
 
-  DLINK_FOREACH(node, conf->leaf_list.head)
-  {
-    if (match(node->data, parv[1]) == 0)
-    {
-      llined = true;
-      break;
-    }
-  }
-
-  DLINK_FOREACH(node, conf->hub_list.head)
-  {
-    if (match(node->data, parv[1]) == 0)
-    {
-      hlined = true;
-      break;
-    }
-  }
 
   /*
    * Ok, this way this works is

src/list.c
@@ -206,6 +206,29 @@ dlinkFind(dlink_list *list, const void *data)
 }
 
 /**
+ * @brief Finds a node with a specific data value using a custom comparison function.
+ *
+ * This function searches for a node with the specified data value in the double-linked list
+ * using a custom comparison function.
+ *
+ * @param list Pointer to the double-linked list.
+ * @param data Pointer to the data value to search for.
+ * @param cmp Function pointer to the comparison function.
+ * @return Pointer to the node with the specified data value, or NULL if not found.
+ */
+dlink_node *
+dlinkFindCmp(const dlink_list *list, const void *data, int (*cmp)(const char *, const char *))
+{
+  dlink_node *node;
+
+  DLINK_FOREACH(node, list->head)
+    if (cmp(node->data, data) == 0)
+      return node;
+
+  return NULL;
+}
+
+/**
  * @brief Moves the contents of one double-linked list to another.
  *
  * This function moves the contents of the specified source double-linked list

commit c11736cf8a7e8af63deb1d09a88737e92764f7dc

- hash.c:list_allow_channel(): fixed regression as pointed out in github issue #43, introduced in a540115

src/hash.c
@@ -513,10 +513,13 @@ free_list_task(struct Client *client)
 static bool
 list_allow_channel(const char *name, const struct ListTask *lt)
 {
+  if (dlink_list_length(&lt->show_mask))
+    if (dlinkFindCmp(&lt->show_mask, name, match) == NULL)
+      return false;
 
-  if (dlinkFindCmp(&lt->show_mask, name, match) == NULL ||
-      dlinkFindCmp(&lt->hide_mask, name, match))
-    return false;
+  if (dlink_list_length(&lt->hide_mask))
+    if (dlinkFindCmp(&lt->hide_mask, name, match))
+      return false;
 
   return true;
 }
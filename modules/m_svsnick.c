b9720769ebcf486c48c238e09b9b3a167919b1be
miwob <wobst.michael@web.de>
2024-04-24 21:52:40 +0000

    - m_svsnick.c: style corrections

--- modules/m_svsnick.c
+++ modules/m_svsnick.c
@@ -81,8 +81,7 @@ ms_svsnick(struct Client *source, int parc, char *parv[])
     {
       sendto_realops_flags(UMODE_SERVNOTICE, L_ALL, SEND_NOTICE,
                            "Received wrong-direction SVSNICK for %s (behind %s) from %s",
-                           target->name, source->from->name,
-                           client_get_name(source, HIDE_IP));
+                           target->name, source->from->name, client_get_name(source, HIDE_IP));
       return;
     }
 

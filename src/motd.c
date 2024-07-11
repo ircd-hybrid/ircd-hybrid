077924f221ffea13d280f6cbe7090c5feddcbd93
miwob <wobst.michael@web.de>
2024-07-11 19:11:07 +0000

    - motd.c: stylistical changes

--- src/motd.c
+++ src/motd.c
@@ -78,9 +78,7 @@ motd_create(const char *mask, const char *path)
     }
   }
 
-  if (mask)
-    motd->mask = io_strdup(mask);
-
+  motd->mask = mask ? io_strdup(mask) : NULL;
   motd->path = io_strdup(path);
   motd->maxcount = MOTD_MAXLINES;
 
@@ -419,23 +417,21 @@ motd_report(struct Client *client, int parc, char *parv[])
 void
 motd_memory_count(struct Client *client)
 {
-  unsigned int mt  = 0;  /* Motd count */
-  unsigned int mtc = 0;  /* Motd cache count */
-  size_t mtm  = 0;  /* Memory consumed by motd */
-  size_t mtcm = 0;  /* Memory consumed by motd cache */
+  unsigned int mt = 0;  /* Motd count */
+  size_t mtm = 0;  /* Memory consumed by motd */
 
   if (MotdList.local)
   {
     ++mt;
     mtm += sizeof(struct Motd);
-    mtm += MotdList.local->path ? (strlen(MotdList.local->path) + 1) : 0;
+    mtm += MotdList.local->path ? strlen(MotdList.local->path) + 1 : 0;
   }
 
   if (MotdList.remote)
   {
     ++mt;
     mtm += sizeof(struct Motd);
-    mtm += MotdList.remote->path ? (strlen(MotdList.remote->path) + 1) : 0;
+    mtm += MotdList.remote->path ? strlen(MotdList.remote->path) + 1 : 0;
   }
 
   list_node_t *node;
@@ -444,16 +440,19 @@ motd_memory_count(struct Client *client)
     const struct Motd *motd = node->data;
     ++mt;
     mtm += sizeof(struct Motd);
-    mtm += motd->path ? (strlen(motd->path) + 1) : 0;
-    mtm += motd->mask ? (strlen(motd->mask) + 1) : 0;
+    mtm += motd->path ? strlen(motd->path) + 1 : 0;
+    mtm += motd->mask ? strlen(motd->mask) + 1 : 0;
   }
+
+  unsigned int mtc = 0;  /* Motd cache count */
+  size_t mtcm = 0;  /* Memory consumed by motd cache */
 
   LIST_FOREACH(node, MotdList.cachelist.head)
   {
     const struct MotdCache *cache = node->data;
     ++mtc;
     mtcm += sizeof(struct MotdCache) + (MOTD_LINESIZE * (cache->count - 1));
-    mtcm += cache->path ? (strlen(cache->path) + 1) : 0;
+    mtcm += cache->path ? strlen(cache->path) + 1 : 0;
   }
 
   sendto_one_numeric(client, &me, RPL_STATSDEBUG | SND_EXPLICIT, "z :Motds %u(%zu) Cache %u(%zu)",

d571f6a7a80fc27316425e3d736611cb8f502f2c
miwob <wobst.michael@web.de>
2024-07-12 19:37:41 +0000

    - extban.h: reorder function declarations

--- src/extban.h
+++ src/extban.h
@@ -79,11 +79,11 @@ extern struct Extban extban_usermode;
 extern void extban_init(void);
 extern void extban_add(struct Extban *);
 extern void extban_del(struct Extban *);
-extern struct Extban *extban_find(unsigned char);
-extern struct Extban *extban_find_flag(unsigned int);
 extern enum extban_type extban_parse(const char *, unsigned int *, unsigned int *);
 extern size_t extban_format(unsigned int, char *);
 extern unsigned int extban_matching_mask(void);
 extern unsigned int extban_acting_mask(void);
+extern struct Extban *extban_find(unsigned char);
+extern struct Extban *extban_find_flag(unsigned int);
 extern const char *extban_get_isupport(void);
 #endif  /* INCLUDED_extban_h */

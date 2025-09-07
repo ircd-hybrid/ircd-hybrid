000cd59bcdd5d30e9971286e67493dda5f093310
miwob <wobst.michael@web.de>
2025-09-07 15:34:12 +0000

    - m_resync.c: harden S2S `RESYNC` handler by enforcing `args_min`

--- modules/m_resync.c
+++ modules/m_resync.c
@@ -65,7 +65,7 @@ static struct Command command_table =
   .name = "RESYNC",
   .handlers[UNREGISTERED_HANDLER] = { .handler = m_unregistered  },
   .handlers[CLIENT_HANDLER] = { .handler = m_ignore },
-  .handlers[SERVER_HANDLER] = { .handler = ms_resync },
+  .handlers[SERVER_HANDLER] = { .handler = ms_resync, .args_min = 2 },
   .handlers[ENCAP_HANDLER] = { .handler = m_ignore },
   .handlers[OPER_HANDLER] = { .handler = m_ignore }
 };

f86a0dd032feb5991a0a12814707d94b60c8af78
Michael Wobst <wobst.michael@web.de>
2026-04-01 22:50:40 +0000

    - m_etrace.c:do_etrace(): use `client_is_local` instead of `MyClient`

--- modules/m_etrace.c
+++ modules/m_etrace.c
@@ -72,7 +72,7 @@ do_etrace(struct Client *source, const char *name)
     doall = true;
   else if (match(name, me.name) == 0)
     doall = true;
-  else if (!MyClient(source) && strcmp(name, me.id) == 0)
+  else if (!client_is_local(source) && strcmp(name, me.id) == 0)
     doall = true;
 
   list_node_t *node;

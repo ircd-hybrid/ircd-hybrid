f66fee5069a7e39b09906e0a595bdaf6111a1f74
Michael Wobst <wobst.michael@web.de>
2026-05-17 11:46:17 +0000

    - m_sjoin.c:ms_sjoin(): treat empty topic strings as unset

--- modules/m_sjoin.c
+++ modules/m_sjoin.c
@@ -315,7 +315,7 @@ ms_sjoin(struct Client *source, int parc, char *parv[])
 
     channel_set_mode_lock(source, channel, NULL);
 
-    if (channel->topic)
+    if (!string_is_empty(channel->topic))
     {
       channel_set_topic(channel, NULL, NULL, 0, false);
       sendto_channel_local(NULL, channel, 0, 0, 0, ":%s TOPIC %s :",
